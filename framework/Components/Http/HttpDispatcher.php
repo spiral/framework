d63eca8951788def16b3874c18decb6a489af547
Wolfy-J <wolfy.jd@gmail.com>
2015-03-07 19:06:08 +0300

    Removed unfinished PSR7 classes.

--- framework/Components/Http/HttpDispatcher.php
+++ framework/Components/Http/HttpDispatcher.php
@@ -8,16 +8,16 @@
  */
 namespace Spiral\Components\Http;
 
-use Psr\Http\Message\StreamableInterface;
+//use Psr\Http\Message\StreamableInterface;
 use Spiral\Components\Debug\ExceptionSnapshot;
-use Spiral\Components\Http\Response\JsonResponse;
+//use Spiral\Components\Http\Response\JsonResponse;
 use Spiral\Core\Component;
 use Spiral\Core\Core;
 use Spiral\Core\Dispatcher\ClientException;
 use Spiral\Core\DispatcherInterface;
 use Spiral\Helpers\StringHelper;
-use Psr\Http\Message\ResponseInterface as PsrResponse;
-use Psr\Http\Message\RequestInterface as PsrRequest;
+//use Psr\Http\Message\ResponseInterface as PsrResponse;
+//use Psr\Http\Message\RequestInterface as PsrRequest;
 
 class HttpDispatcher extends Component implements DispatcherInterface
 {
@@ -42,87 +42,87 @@ class HttpDispatcher extends Component implements DispatcherInterface
         //dispatch
     }
 
-    public function perform(PsrRequest $request)
-    {
-        //Create request scope ? or no?
-        //if so, scope for what request type, only our? i think yes.
-
-        if ($request instanceof RequestInterface)
-        {
-            //making scope, maybe make scope INSIDE route with route attached to request AS data chunk?
-        }
-
-        //perform, INNER MIDDLEWARE INSIDE ROUTE! i need RouterTrait! :)
-        $response = null;
-
-        //End request scope ? or no?
-        if ($request instanceof RequestInterface)
-        {
-            //ending scope
-        }
-
-        return $this->wrapResponse($response);
-    }
-
-    protected function wrapResponse($response)
-    {
-        if ($response instanceof PsrResponse)
-        {
-            return $response;
-        }
-
-        //check situation where response is actually stream
-        if ($response instanceof StreamableInterface)
-        {
-            //something like that
-            return new Response($response);
-        }
-
-        if (is_array($response) || $response instanceof \JsonSerializable)
-        {
-            //Making json response
-            return new JsonResponse($response); //something like this
-        }
-
-        //Making base response (string)
-        return $response;
-    }
-
-    public function dispatch(PsrResponse $response, PsrRequest $request = null)
-    {
-        //do we need request here?
-
-        //Sending headers and status
-
-        //sending status
-
-        foreach ($response->getHeaders() as $header)
-        {
-            //sending header
-        }
-
-        if ($response instanceof ResponseInterface)
-        {
-            //Sending cookies
-            foreach ($response->getCookies() as $cookie)
-            {
-                //setcookie
-            }
-        }
-
-        //Sending stream
-        $stream = $response->getBody();
-        if (!$stream->isSeekable())
-        {
-            echo $stream->getContents();
-        }
-        else
-        {
-            //seeking
-        }
-
-        exit();
-    }
+    //    public function perform(PsrRequest $request)
+    //    {
+    //        //Create request scope ? or no?
+    //        //if so, scope for what request type, only our? i think yes.
+    //
+    //        if ($request instanceof RequestInterface)
+    //        {
+    //            //making scope, maybe make scope INSIDE route with route attached to request AS data chunk?
+    //        }
+    //
+    //        //perform, INNER MIDDLEWARE INSIDE ROUTE! i need RouterTrait! :)
+    //        $response = null;
+    //
+    //        //End request scope ? or no?
+    //        if ($request instanceof RequestInterface)
+    //        {
+    //            //ending scope
+    //        }
+    //
+    //        return $this->wrapResponse($response);
+    //    }
+    //
+    //    protected function wrapResponse($response)
+    //    {
+    //        if ($response instanceof PsrResponse)
+    //        {
+    //            return $response;
+    //        }
+    //
+    //        //check situation where response is actually stream
+    //        if ($response instanceof StreamableInterface)
+    //        {
+    //            //something like that
+    //            return new Response($response);
+    //        }
+    //
+    //        if (is_array($response) || $response instanceof \JsonSerializable)
+    //        {
+    //            //Making json response
+    //            return new JsonResponse($response); //something like this
+    //        }
+    //
+    //        //Making base response (string)
+    //        return $response;
+    //    }
+    //
+    //    public function dispatch(PsrResponse $response, PsrRequest $request = null)
+    //    {
+    //        //do we need request here?
+    //
+    //        //Sending headers and status
+    //
+    //        //sending status
+    //
+    //        foreach ($response->getHeaders() as $header)
+    //        {
+    //            //sending header
+    //        }
+    //
+    //        if ($response instanceof ResponseInterface)
+    //        {
+    //            //Sending cookies
+    //            foreach ($response->getCookies() as $cookie)
+    //            {
+    //                //setcookie
+    //            }
+    //        }
+    //
+    //        //Sending stream
+    //        $stream = $response->getBody();
+    //        if (!$stream->isSeekable())
+    //        {
+    //            echo $stream->getContents();
+    //        }
+    //        else
+    //        {
+    //            //seeking
+    //        }
+    //
+    //        exit();
+    //    }
 
     /**
      * Every dispatcher should know how to handle exception snapshot provided by Debugger.
@@ -135,7 +135,7 @@ class HttpDispatcher extends Component implements DispatcherInterface
         if ($snapshot->getException() instanceof ClientException)
         {
             //Simply showing something
-            $this->dispatch(new Response('ERROR VIEW LAYOUT IF PRESENTED', $snapshot->getException()->getCode()));
+            //$this->dispatch(new Response('ERROR VIEW LAYOUT IF PRESENTED', $snapshot->getException()->getCode()));
         }
 
         echo $snapshot->renderSnapshot();
