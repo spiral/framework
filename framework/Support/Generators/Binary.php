0ac02fef2f7a0aa0cef42c736c8b07f0323307ab
Wolfy-J <wolfy.jd@gmail.com>
2015-03-12 18:12:18 +0300

    Minor style corrections.

--- framework/Support/Generators/Binary.php
+++ framework/Support/Generators/Binary.php
@@ -13,8 +13,8 @@ use Spiral\Core\Component;
 class Binary extends Component
 {
     /**
-     * Predefined file modes, Binary reader/writer will work only in binary modes, hoverer it can append content to already
-     * created file.
+     * Predefined file modes, Binary reader/writer will work only in binary modes, hoverer it can
+     * append content to already created file.
      */
     const MODE_WRITE  = 'wb';
     const MODE_APPEND = 'ab';
@@ -28,7 +28,8 @@ class Binary extends Component
     protected $mode = self::MODE_WRITE;
 
     /**
-     * Context filename where data will be written or reader from. File will be created automatically on object construction.
+     * Context filename where data will be written or reader from. File will be created automatically
+     * on object construction.
      *
      * @var bool|string
      */
@@ -42,17 +43,19 @@ class Binary extends Component
     protected $handler = false;
 
     /**
-     * If enabled file will be processed using streamed gzip encoding, should should make output dramatically smaller.
+     * If enabled file will be processed using streamed gzip encoding, should should make output
+     * dramatically smaller.
      *
      * @var bool
      */
     protected $compress = false;
 
     /**
-     * Create new Binary instance. Binary generally used to transfer or store big amount of data in plain file, where data
-     * structure is undefined and database is not required. Same class can read and write binary files. Last construction
-     * option specifies compressing level, if enabled (values 1-9) file will be written using gzip stream.
-     * You have to provide compress option while reading compressed files.
+     * Create new Binary instance. Binary generally used to transfer or store big amount of data in
+     * plain file, where data structure is undefined and database is not required. Same class can
+     * read and write binary files. Last construction option specifies compressing level, if enabled
+     * (values 1-9) file will be written using gzip stream. You have to provide compress option while
+     * reading compressed files.
      *
      * @param string $filename Local filename or URL (if allowed by PHP).
      * @param string $mode     Read or write mode, values supported - wb, rb, ab. Write by default.
@@ -65,18 +68,21 @@ class Binary extends Component
         $this->compress = $compress;
         $this->mode = $mode;
 
-        $this->handler = $this->compress ? gzopen($this->filename, $this->mode) : fopen($this->filename, $this->mode);
-        if (!$this->handler)
+        $this->handler = $this->compress
+            ? gzopen($this->filename, $this->mode)
+            : fopen($this->filename, $this->mode);
+
+        if (empty($this->handler))
         {
             throw new \Exception('Error during opening/creating file.');
         }
     }
 
     /**
-     * Write or append data to open file handler. This function can be used only if append or write mode selected. Data
-     * should be either binary or string blob. To encode more complex structures use json_encode or serialize functions.
-     * Every data chunk will be prepended with length value which will allow Binary class read exactly same amount of data
-     * was written to file.
+     * Write or append data to open file handler. This function can be used only if append or write
+     * mode selected. Data should be either binary or string blob. To encode more complex structures
+     * use json_encode or serialize functions. Every data chunk will be prepended with length value
+     * which will allow Binary class read exactly same amount of data was written to file.
      *
      * @param string $data Data blob.
      * @return int
@@ -86,17 +92,17 @@ class Binary extends Component
     {
         if ($this->mode != self::MODE_WRITE && $this->mode != self::MODE_APPEND)
         {
-            throw new \Exception('Unable to write data, read mode is set.');
+            throw new \RuntimeException('Unable to write data, read mode is set.');
         }
 
-        if (!$this->handler)
+        if (empty($this->handler))
         {
-            throw new \Exception('Unable to add data to file, no resource available.');
+            throw new \RuntimeException('Unable to add data to file, no resource available.');
         }
 
         $packedLength = pack('L', strlen($data));
 
-        if ($this->compress)
+        if (!(bool)$this->compress)
         {
             return gzwrite($this->handler, $packedLength . $data);
         }
@@ -105,10 +111,11 @@ class Binary extends Component
     }
 
     /**
-     * Read one portion of data in binary file. This function can be used only if read select. Resulted data will alway
-     * s be represented as string (blob) and can be unpacked with traditional methods like json_decode or unserialize.
-     * Make sure compress option is enabled if original file was created using gzip stream. There is no memory limits on
-     * how big binary file can be, hoverer one data portion should fit to PHP memory.
+     * Read one portion of data in binary file. This function can be used only if read select.
+     * Resulted data will always be represented as string (blob) and can be unpacked with traditional
+     * methods like json_decode or unserialize. Make sure compress option is enabled if original file
+     * was created using gzip stream. There is no memory limits on how big binary file can be, hoverer
+     * one data portion should fit to PHP memory.
      *
      * @return string
      * @throws \Exception
@@ -117,15 +124,15 @@ class Binary extends Component
     {
         if ($this->mode != self::MODE_READ)
         {
-            throw new \Exception('Unable to read data, write/append mode is set.');
+            throw new \RuntimeException('Unable to read data, write/append mode is set.');
         }
 
-        if (!$this->handler)
+        if (empty($this->handler))
         {
-            throw new \Exception('Unable to read data from file, no resource available.');
+            throw new \RuntimeException('Unable to read data from file, no resource available.');
         }
 
-        if ($this->compress)
+        if (!(bool)$this->compress)
         {
             $length = gzread($this->handler, 4);
         }
@@ -134,7 +141,7 @@ class Binary extends Component
             $length = fread($this->handler, 4);
         }
 
-        if (!$length)
+        if (empty($length))
         {
             return null;
         }
@@ -146,7 +153,7 @@ class Binary extends Component
 
         $length = unpack('L', $length);
 
-        if ($this->compress)
+        if ((bool)$this->compress)
         {
             return gzread($this->handler, $length[1]);
         }
@@ -161,9 +168,9 @@ class Binary extends Component
      */
     public function close()
     {
-        if ($this->handler)
+        if (!empty($this->handler))
         {
-            if ($this->compress)
+            if ((bool)$this->compress)
             {
                 gzclose($this->handler);
             }
