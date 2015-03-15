49f5c4a087eed659113282ef3b3f9c8f03926dfe
Wolfy-J <wolfy.jd@gmail.com>
2015-03-15 23:44:59 +0300

    Active path value.

---
+++ framework/Components/Http/Router/RouterException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Components\Http\Router;
+
+use Spiral\Core\CoreException;
+
+class RouterException extends CoreException
+{
+
+}
