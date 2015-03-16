e4c220f88fa0fb5438baa4d7f0f2fedffb247eec
Wolfy-J <wolfy.jd@gmail.com>
2015-03-17 01:07:44 +0300

    Redirect response.

---
+++ framework/Components/Http/Response/RedirectResponse.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Components\Http\Response;
+
+use Psr\Http\Message\UriInterface;
+use Spiral\Components\Http\Message\Stream;
+use Spiral\Components\Http\Response;
+
+class RedirectResponse extends Response
+{
+    /**
+     * Redirect response.
+     *
+     * @param string|UriInterface $uri
+     * @param int                 $status
+     */
+    public function __construct($uri, $status = self::REDIRECT)
+    {
+        parent::__construct(new Stream(), $status, array(
+            'Location' => (string)$uri
+        ));
+    }
+}
