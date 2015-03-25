2e9cd3d159e4b5486ae18b3ebb7469535c3ea39b
Wolfy-J <wolfy.jd@gmail.com>
2015-03-26 01:22:47 +0300

    minor refactoring.

--- framework/Components/Debug/Logger.php
+++ framework/Components/Debug/Logger.php
@@ -8,17 +8,16 @@
  */
 namespace Spiral\Components\Debug;
 
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
+use Psr\Log\AbstractLogger;
 use Spiral\Components\Files\FileManager;
 use Spiral\Core\Component;
 
-class Logger extends Component implements LoggerInterface
+class Logger extends AbstractLogger
 {
     /**
      * Events for logging.
      */
-    use Component\EventsTrait;
+    use Component\ComponentTrait, Component\EventsTrait;
 
     /**
      * Helper constant to associate all log levels with one filename.
@@ -163,109 +162,6 @@ class Logger extends Component implements LoggerInterface
         $this->fileHandlers[$level] = array($filename, $filesize);
 
         return $this;
-    }
-
-    /**
-     * System is unusable.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function emergency($message, array $context = array())
-    {
-        return $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    /**
-     * Action must be taken immediately.
-     * Example: Entire website down, database unavailable, etc. This should trigger the SMS alerts
-     * and wake you up.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function alert($message, array $context = array())
-    {
-        return $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    /**
-     * Critical conditions.
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function critical($message, array $context = array())
-    {
-        return $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically be logged and monitored.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function error($message, array $context = array())
-    {
-        return $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    /**
-     * Exceptional occurrences that are not errors.
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things that are not necessarily
-     * wrong.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function warning($message, array $context = array())
-    {
-        return $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function notice($message, array $context = array())
-    {
-        return $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function info($message, array $context = array())
-    {
-        return $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array  $context
-     * @return static
-     */
-    public function debug($message, array $context = array())
-    {
-        return $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     /**
