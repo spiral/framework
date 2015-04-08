50934da1ccda1e7a38923e8847cef257b5ce5a83
Wolfy-J <wolfy.jd@gmail.com>
2015-04-08 21:58:42 +0300

    orm basics

---
+++ framework/Components/ORM/ORM.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Components\ORM;
+
+use Spiral\Core\Component;
+use Spiral\Core\Core;
+
+class ORM extends Component
+{
+    /**
+     * Required traits.
+     */
+    use Component\SingletonTrait, Component\ConfigurableTrait, Component\EventsTrait;
+
+    /**
+     * Declares to IoC that component instance should be treated as singleton.
+     */
+    const SINGLETON = 'orm';
+
+    /**
+     * ORM component instance.
+     *
+     * @param Core $core
+     */
+    public function __construct(Core $core)
+    {
+        $this->core = $core;
+        $this->config = $core->loadConfig('orm');
+    }
+}
