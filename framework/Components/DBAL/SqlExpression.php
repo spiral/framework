c2093a346b645c8d69d5694c329506d76268d70c
Wolfy-J <wolfy.jd@gmail.com>
2015-05-09 16:08:09 +0300

    more descriptive comment for sql expression

--- framework/Components/DBAL/SqlExpression.php
+++ framework/Components/DBAL/SqlExpression.php
@@ -11,7 +11,14 @@ namespace Spiral\Components\DBAL;
 class SqlExpression extends SqlFragment
 {
     /**
-     * Get or render SQL statement.
+     * Get or render SQL statement. SQLExpression is different than SQLFragment as it content will
+     * be quoted and prefixed using QueryCompiler.
+     *
+     * Example use:
+     * new SQLExpression("table.column_a + table.column_b")
+     *
+     * Upon query compilation expression will be converted to valid sql:
+     * ... "prefix_table.column_a" + "prefix_table.column_b"
      *
      * @param QueryCompiler $compiler
      * @return string
