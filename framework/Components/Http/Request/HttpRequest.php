4f49b3e272924184b5c8471a0696bbc8cc61c587
Wolfy-J <wolfy.jd@gmail.com>
2015-05-25 00:10:09 +0300

    psr 7 refactoring

--- framework/Components/Http/Request/HttpRequest.php
+++ framework/Components/Http/Request/HttpRequest.php
@@ -96,14 +96,93 @@ class HttpRequest extends HttpMessage implements RequestInterface
             throw new \InvalidArgumentException("Unsupported HTTP method value provided.");
         }
 
+        $this->method = $method;
+
         if (!empty($uri))
         {
             $this->uri = ($uri instanceof UriInterface) ? $uri : new Uri($uri);
         }
 
-        $this->method = $method;
         $this->body = ($body instanceof StreamInterface) ? $body : new Stream($body);
-        $this->headers = $this->normalizeHeaders($headers);
+        list($this->headers, $this->normalizedHeaders) = $this->normalizeHeaders($headers);
+    }
+
+    /**
+     * Retrieves all message header values.
+     *
+     * The keys represent the header name as it will be sent over the wire, and
+     * each value is an array of strings associated with the header.
+     *
+     *     // Represent the headers as a string
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         echo $name . ": " . implode(", ", $values);
+     *     }
+     *
+     *     // Emit headers iteratively:
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         foreach ($values as $value) {
+     *             header(sprintf('%s: %s', $name, $value), false);
+     *         }
+     *     }
+     *
+     * While header names are not case-sensitive, getHeaders() will preserve the
+     * exact case in which headers were originally specified.
+     *
+     * @return array Returns an associative array of the message's headers. Each
+     *     key MUST be a header name, and each value MUST be an array of strings
+     *     for that header.
+     */
+    public function getHeaders()
+    {
+        $result = $this->headers;
+        if (!$this->hasHeader('host') && (!empty($this->uri) && $this->uri->getHost()))
+        {
+            $headers['Host'] = array($this->getUriHost());
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * This method returns an array of all the header values of the given
+     * case-insensitive header name.
+     *
+     * If the header does not appear in the message, this method MUST return an
+     * empty array.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string[] An array of string values as provided for the given
+     *                     header. If the header does not appear in the message, this method MUST
+     *                     return an empty array.
+     */
+    public function getHeader($name)
+    {
+        if (!$this->hasHeader($name))
+        {
+            if (strtolower($name) == 'host' && (!empty($this->uri) && $this->uri->getHost()))
+            {
+                return array($this->getUriHost());
+            }
+
+            return array();
+        }
+
+        return parent::getHeader($name);
+    }
+
+    /**
+     * Fetch host value from uri.
+     *
+     * @return string
+     */
+    private function getUriHost()
+    {
+        $host = $this->uri->getHost();
+        $host .= $this->uri->getPort() ? ':' . $this->uri->getPort() : '';
+
+        return $host;
     }
 
     /**
