d15237703ab4ec069f04696a7b70146c63d34aa7
Wolfy-J <wolfy.jd@gmail.com>
2015-07-11 12:48:30 +0300

    Native importer.

--- source/Components/View/Compiler/Processors/Templater/Importers/NativeImporter.php
+++ source/Components/View/Compiler/Processors/Templater/Importers/NativeImporter.php
@@ -8,9 +8,80 @@
  */
 namespace Spiral\Components\View\Compiler\Processors\Templater\Importers;
 
+use Spiral\Components\View\Compiler\Compiler;
+use Spiral\Components\View\Compiler\Processors\TemplateProcessor;
 use Spiral\Components\View\Compiler\Processors\Templater\ImporterInterface;
 
 class NativeImporter implements ImporterInterface
 {
+    /**
+     * Html tag name.
+     *
+     * @var string
+     */
+    protected $element = '';
 
+    /**
+     * Is importer definitive.
+     *
+     * @var bool
+     */
+    protected $definitive = false;
+
+    /**
+     * New instance of importer.
+     *
+     * @param Compiler          $compiler
+     * @param TemplateProcessor $templater
+     * @param array             $options
+     */
+    public function __construct(Compiler $compiler, TemplateProcessor $templater, array $options)
+    {
+        $this->element = $options['native'];
+        $this->definitive = array_key_exists('definitive', $options);
+    }
+
+    /**
+     * Definitive imports allows developer to create custom element aliases in a scope of element
+     * import (sub-tags).
+     *
+     * @return bool
+     */
+    public function isDefinitive()
+    {
+        return $this->definitive;
+    }
+
+    /**
+     * Check if element (tag) has to be imported.
+     *
+     * @param string $element
+     * @return bool
+     */
+    public function isImported($element)
+    {
+        return strtolower($element) == strtolower($this->element);
+    }
+
+    /**
+     * Get imported element namespace.
+     *
+     * @param string $element
+     * @return string
+     */
+    public function getNamespace($element)
+    {
+        return null;
+    }
+
+    /**
+     * Get imported element view name.
+     *
+     * @param string $element
+     * @return string
+     */
+    public function getView($element)
+    {
+        return null;
+    }
 }
