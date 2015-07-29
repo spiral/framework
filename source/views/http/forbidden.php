357c41b3bc002e711b2427b5f302cd2895e3c95c
Wolfy-J <wolfy.jd@gmail.com>
2015-07-29 23:57:17 +0300

    views

---
+++ source/views/http/forbidden.php
@@ -0,0 +1,6 @@
+<extends:http.layouts.error code="403" title="[[Forbidden]]"/>
+
+<block:message>
+    [[You do not have permission to access this page. Please contact the webmaster, for additional
+    information.]]
+</block:message>
---
+++ source/views/http/notFound.php
@@ -0,0 +1,14 @@
+<extends:http.layouts.error code="404" title="[[This page cannot be found]]"/>
+
+<block:message>
+    <?php
+    /**
+     * Expecting error to happen inside request scope declared by HttpDispatcher in perform() method.
+     *
+     * @var \Spiral\Components\Http\Uri $uri
+     */
+    $uri = \Spiral\Core\Container::getInstance()->get('request')->getUri();
+    ?>
+    [[The requested URL]] <a href="<?= $uri->getPath() ?>"><?= $uri->getPath() ?></a> [[was not
+    found on this server.]]
+</block:message>
