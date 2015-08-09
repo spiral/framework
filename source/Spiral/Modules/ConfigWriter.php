639224af189c49be8c403674eecd6860ceaaab8d
Wolfy-J <wolfy.jd@gmail.com>
2015-08-09 17:12:05 +0300

    config serializer

---
+++ source/Spiral/Modules/ConfigWriter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Modules;
+
+class ConfigWriter
+{
+
+}
---
+++ source/Spiral/Modules/Exceptions/ConfigWriterException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Modules\Exceptions;
+
+use Spiral\Core\Exceptions\RuntimeException;
+
+/**
+ * Error while configuration file update.
+ */
+class ConfigWriterException extends RuntimeException
+{
+
+}
