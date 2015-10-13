d4d1459da856edfbeedbca3bff24e864f9d8897f
Wolfy-J <wolfy.jd@gmail.com>
2015-10-13 23:18:36 +0300

    population

--- source/Spiral/Reactor/Generators/RequestGenerator.php
+++ source/Spiral/Reactor/Generators/RequestGenerator.php
@@ -148,6 +148,10 @@ class RequestGenerator extends AbstractService
         ]);
         $populate->parameter('entity')->setType('EntityInterface');
         $populate->setSource([
+            "if(!\$this->isValid()) {",
+            "   return false;",
+            "}",
+            "",
             "//Populating entity fields and fetching it's errors",
             "parent::populate(\$entity);",
             "",
