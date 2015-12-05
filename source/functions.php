0b65d4742b0cbcec251e057df4f38c271d22a779
Wolfy-J <wolfy.jd@gmail.com>
2015-12-05 03:35:05 +0300

    env function

--- source/functions.php
+++ source/functions.php
@@ -139,3 +139,41 @@ if (!function_exists('p')) {
         return spiral(TranslatorInterface::class)->transChoice($string, $number, $options, $domain);
     }
 }
+
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'null':
+            case '(null)':
+                return null;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+        }
+
+        return $value;
+    }
+}
