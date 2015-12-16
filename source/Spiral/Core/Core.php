201d806769a11d6f3a69b20813ea149d6f3b5594
Wolfy-J <wolfy.jd@gmail.com>
2015-12-17 00:22:38 +0300

    dispatcherless exceptions fix

--- source/Spiral/Core/Core.php
+++ source/Spiral/Core/Core.php
@@ -342,7 +342,7 @@ abstract class Core extends Component implements CoreInterface, DirectoriesInter
             //Now dispatcher can handle snapshot it's own way
             $this->dispatcher->handleSnapshot($snapshot);
         } else {
-            echo $snapshot;
+            echo $snapshot->exception();
         }
     }
 
