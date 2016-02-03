d97129c81d23f2e12ee6c645f69f8d5998b1ebdb
Wolfy-J <wolfy.jd@gmail.com>
2016-02-03 21:55:16 +0300

    more tests

---
+++ tests/Cases/Core/ServicesTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+namespace Spiral\Tests\Cases\Core;
+
+use Spiral\Core\Container;
+use Spiral\Core\Service;
+
+class ServicesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \Spiral\Core\Exceptions\SugarException
+     */
+    public function testPropertiesFailure()
+    {
+        $container = new Container();
+        $service = new ServiceFixture($container);
+
+        $service->getAbc();
+    }
+
+    public function testPropertiesSuccess()
+    {
+        $container = new Container();
+        $container->bind('abc', function () {
+            return 'test';
+        });
+
+        $service = new ServiceFixture($container);
+        $this->assertEquals('test', $service->getAbc());
+    }
+}
+
+class ServiceFixture extends Service
+{
+    public function getAbc()
+    {
+        return $this->abc;
+    }
+}
