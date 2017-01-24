0a5c881788bb945644371bc0c461cb903ddd5ae9
Wolfy-J <wolfy.jd@gmail.com>
2017-01-24 20:22:45 +0300

    more tests

--- tests/Translator/IndexerTest.php
+++ tests/Translator/IndexerTest.php
@@ -7,21 +7,15 @@
 
 namespace Spiral\Tests\Translator;
 
-use Spiral\Console\ConsoleDispatcher;
 use Spiral\Tests\BaseTest;
 
 class IndexerTest extends BaseTest
 {
     public function testIndex()
     {
-        $this->app->translator->loadLocales();
-
-        $this->assertSame(
-            ConsoleDispatcher::CODE_UNDEFINED,
-            $this->app->console->run('i18n:index')->getCode()
+        $this->assertSame(0, $this->app->console->run('i18n:index')->getCode());
+        $this->assertNotEmpty(
+            $this->app->translator->getCatalogue()->domainMessages('validation')
         );
-
-        //todo: fix it
-        print_r($this->app->translator->getCatalogue()->domainMessages('validation'));
     }
 }
