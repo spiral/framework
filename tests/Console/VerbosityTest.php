4b6441d22a1da02c0a781186fe6781aa25a22e95
Wolfy-J <wolfy.jd@gmail.com>
2017-01-25 00:06:07 +0300

    more tests

--- tests/Console/VerbosityTest.php
+++ tests/Console/VerbosityTest.php
@@ -8,15 +8,23 @@
 namespace Spiral\Tests\Console;
 
 use Spiral\Tests\BaseTest;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class VerbosityTest extends BaseTest
 {
     public function testConfigureWithVerbosity()
     {
-        $output = $this->console->run('configure', [
-            '-vv' => true
-        ])->getOutput()->fetch();
+        $output = new BufferedOutput();
+        $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
 
-        $this->assertContains('All done!', $output);
+        $this->console->run('configure', [], $output);
+        $result = $output->fetch();
+
+        //Expect to be added via loggers
+        $this->assertContains(
+            '[Indexer] Found [validation]: \'Condition \'{condition}\' does not meet.\'',
+            $result
+        );
     }
 }
