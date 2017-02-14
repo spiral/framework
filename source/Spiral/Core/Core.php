2535ca9b88b472d04aed170ea489f059ff74bc29
Wolfy-J <wolfy.jd@gmail.com>
2017-02-14 19:46:42 +0300

    fix container dependency

--- source/Spiral/Core/Core.php
+++ source/Spiral/Core/Core.php
@@ -112,6 +112,11 @@ abstract class Core extends AbstractCore implements DirectoriesInterface
     protected $load = [];
 
     /**
+     * @var \Spiral\Core\ContainerInterface
+     */
+    protected $container;
+
+    /**
      * Core class will extend default spiral container and initiate set of directories. You must
      * provide application, libraries and root directories to constructor.
      *
