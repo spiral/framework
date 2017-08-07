cf5e186ea4f8bdd9a7e4d9cd4e5b9978fe81ca55
Lev Seleznev <lev@spiralscout.com>
2017-08-07 15:03:46 +0300

    View cache file locator

--- tests/Views/StemplerTest.php
+++ tests/Views/StemplerTest.php
@@ -10,10 +10,20 @@ namespace Spiral\Tests\Views;
 use Spiral\Tests\BaseTest;
 use Spiral\Views\Engines\Stempler\StemplerCache;
 use Spiral\Views\Engines\Stempler\StemplerView;
+use Spiral\Views\ViewCacheLocator;
 use Spiral\Views\ViewLoader;
 
 class StemplerTest extends BaseTest
 {
+    protected function deleteCacheFiles()
+    {
+        foreach ($this->files->getFiles($this->views->getEnvironment()->cacheDirectory()) as $filename) {
+            //If exception is thrown here this will mean that application wasn't correctly
+            //destructed and there is open resources kept
+            $this->files->delete($filename);
+        }
+    }
+
     public function testRenderSimple()
     {
         $this->assertSame('Hello, World!', trim($this->views->render('home', [
@@ -91,5 +101,28 @@ class StemplerTest extends BaseTest
     public function testView()
     {
         $this->assertInstanceOf(StemplerView::class, $this->views->get('home'));
+    }
+
+    public function testCacheLocator()
+    {
+        $this->deleteCacheFiles();
+        clearstatcache();
+
+        $this->views->withEnvironment(
+            $this->views->getEnvironment()->withDependency('value', function () {
+                return 'test-one';
+            })
+        )->compile('home');
+
+        $this->views->withEnvironment(
+            $this->views->getEnvironment()->withDependency('value', function () {
+                return 'test-two';
+            })
+        )->compile('home');
+
+        /** @var ViewCacheLocator $data */
+        $viewCacheLocator = $this->container->get(ViewCacheLocator::class);
+
+        $this->assertSame(2, count($viewCacheLocator->getFiles('home')));
     }
 }
