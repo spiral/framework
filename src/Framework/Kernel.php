985a89f050d53f1ffc52555e11f39c5f1f52904f
Wolfy-J <wolfy.jd@gmail.com>
2018-09-15 17:04:26 +0300

    initial components

--- src/Framework/Kernel.php
+++ src/Framework/Kernel.php
@@ -9,8 +9,15 @@
 namespace Spiral\Framework;
 
 use Spiral\Boot\AbstractKernel;
+use Spiral\Boot\Bootloaders\CoreBootloader;
+use Spiral\Debug\Bootloaders\DebugBootloader;
+use Spiral\Tokenizer\Bootloaders\TokenizerBootloader;
 
 abstract class Kernel extends AbstractKernel
 {
-
+    const SYSTEM = [
+        CoreBootloader::class,
+        DebugBootloader::class,
+        TokenizerBootloader::class,
+    ];
 }
