a345339b4de07717a0e6a935d8523ca352c634d4
Wolfy-J <wolfy.jd@gmail.com>
2018-09-16 14:00:33 +0300

    RPC connection init

--- src/RoadRunner/Bootloaders/RoadRunnerBootloader.php
+++ src/RoadRunner/Bootloaders/RoadRunnerBootloader.php
@@ -13,28 +13,17 @@ use Spiral\Boot\KernelInterface;
 use Spiral\Core\Bootloaders\Bootloader;
 use Spiral\Goridge\RPC;
 use Spiral\Goridge\SocketRelay;
+use Spiral\RoadRunner\Exceptions\RoadRunnerException;
 use Spiral\RoadRunner\RoadRunnerDispatcher;
 
 class RoadRunnerBootloader extends Bootloader
 {
-    const BOOT = true;
-
-    const SINGLETONS = [
+    const BOOT        = true;
+    const RPC_DEFAULT = 'tcp://127.0.0.1:6001';
+    const SINGLETONS  = [
         RoadRunnerDispatcher::class => RoadRunnerDispatcher::class,
         RPC::class                  => [self::class, 'rpc']
     ];
-
-    protected function rpc(EnvironmentInterface $environment): RPC
-    {
-        $conn = $environment->get('RR_RPC', 'tcp://localhost:6001');
-
-        // todo: parse conn
-
-
-        $relay = new SocketRelay("127.0.0.1", 6001);
-
-        return new RPC($relay);
-    }
 
     /**
      * @param KernelInterface      $kernel
@@ -47,5 +36,34 @@ class RoadRunnerBootloader extends Bootloader
         if (function_exists('gc_collect_cycles')) {
             $rr->addFinalizer('gc_collect_cycles');
         }
+    }
+
+    /**
+     * @param EnvironmentInterface $environment
+     * @return RPC
+     */
+    protected function rpc(EnvironmentInterface $environment): RPC
+    {
+        $conn = $environment->get('RR_RPC', static::RPC_DEFAULT);
+
+        if (!preg_match('#^([a-z]+)://([^:]+):?(\d+)?$#i', $conn, $parts)) {
+            throw new RoadRunnerException(
+                "Unable to create RPC connection, invalid DSN given `{$conn}`."
+            );
+        }
+
+        if (!in_array($parts[1], ['tcp', 'unix'])) {
+            throw new RoadRunnerException(
+                "Unable to create RPC connection, invalid DSN given `{$conn}`."
+            );
+        }
+
+        if ($parts[1] == 'unix') {
+            $relay = new SocketRelay($parts[2], null, SocketRelay::SOCK_UNIX);
+        } else {
+            $relay = new SocketRelay($parts[2], $parts[3], SocketRelay::SOCK_TCP);
+        }
+
+        return new RPC($relay);
     }
 }
