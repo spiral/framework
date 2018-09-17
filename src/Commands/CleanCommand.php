72a7eaf555f5ea3b80c2e68010b05bba857c6cae
Wolfy-J <wolfy.jd@gmail.com>
2018-09-17 23:12:43 +0300

    some commands added

---
+++ src/Commands/CleanCommand.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Commands;
+
+use Spiral\Boot\DirectoriesInterface;
+use Spiral\Console\Command;
+use Spiral\Files\FilesInterface;
+
+class CleanCommand extends Command
+{
+    const NAME        = 'clean:runtime';
+    const DESCRIPTION = 'Clean application runtime cache';
+
+    /**
+     * @param FilesInterface       $files
+     * @param DirectoriesInterface $directories
+     */
+    public function perform(FilesInterface $files, DirectoriesInterface $directories)
+    {
+        $cacheDirectory = $directories->get('cache');
+        if (!$files->exists($cacheDirectory)) {
+            $this->writeln("Cache directory is missing, no cache to be cleaned.");
+
+            return;
+        }
+
+        if ($this->isVerbose()) {
+            $this->writeln("<info>Cleaning application cache:</info>");
+        }
+
+        foreach ($files->getFiles($cacheDirectory) as $filename) {
+            try {
+                $files->delete($filename);
+            } catch (\Throwable $e) {
+                $this->sprintf("> <fg=red>errored</fg=red> `%s`: <fg=red>%s</fg=red>\n",
+                    $files->relativePath($filename, $cacheDirectory),
+                    $e->getMessage()
+                );
+
+                continue;
+            }
+
+            if ($this->isVerbose()) {
+                $this->sprintf(
+                    "> <fg=green>deleted</fg=green> `%s`\n",
+                    $files->relativePath($filename, $cacheDirectory)
+                );
+            }
+        }
+
+        $this->writeln("<info>Runtime cache has been cleared.</info>");
+    }
+}
---
+++ src/Commands/ExtensionsCommand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Commands;
+
+use Spiral\Console\Command;
+
+class ExtensionsCommand extends Command
+{
+    const NAME        = 'php:extensions';
+    const DESCRIPTION = 'Get list of available php extension';
+
+    /**
+     * Find extensions.
+     */
+    public function perform()
+    {
+        $grid = $this->table(['Extension:', 'Version:']);
+        foreach (get_loaded_extensions() as $extension) {
+            $grid->addRow([$extension, phpversion($extension)]);
+        }
+        $grid->render();
+    }
+}
