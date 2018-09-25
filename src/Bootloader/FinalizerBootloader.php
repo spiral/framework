ec2598b04ca6a55ca8fc7d2be24f18621ec1d237
Wolfy-J <wolfy.jd@gmail.com>
2018-09-25 22:51:19 +0300

    fix finalizer

--- src/Bootloader/FinalizerBootloader.php
+++ src/Bootloader/FinalizerBootloader.php
@@ -14,20 +14,7 @@ use Spiral\Finalizer\FinalizerInterface;
 
 class FinalizerBootloader extends Bootloader
 {
-    const BOOT = true;
-
     const SINGLETONS = [
         FinalizerInterface::class => Finalizer::class,
-        Finalizer::class          => Finalizer::class
     ];
-
-    /**
-     * @param Finalizer $finalizer
-     */
-    public function boot(Finalizer $finalizer)
-    {
-        if (function_exists('gc_collect_cycles')) {
-            $finalizer->addFinalizer('gc_collect_cycles');
-        }
-    }
 }
