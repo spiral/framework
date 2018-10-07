41c53a996a5f290363b1b84d773bda5087a1cc82
Wolfy-J <wolfy.jd@gmail.com>
2018-10-07 17:29:32 +0300

    no more benchmarker

--- src/Bootloader/System/DebugBootloader.php
+++ src/Bootloader/System/DebugBootloader.php
@@ -9,14 +9,11 @@
 namespace Spiral\Bootloader\System;
 
 use Spiral\Core\Bootloader\Bootloader;
-use Spiral\Debug\Benchmarker;
-use Spiral\Debug\BenchmarkerInterface;
 use Spiral\Debug\Dumper;
 
 class DebugBootloader extends Bootloader
 {
     const SINGLETONS = [
-        BenchmarkerInterface::class => Benchmarker::class,
-        Dumper::class               => Dumper::class,
+        Dumper::class => Dumper::class,
     ];
 }
