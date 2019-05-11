3053256268c9f3324aa5408a65858b66c130e972
Wolfy-J <wolfy.jd@gmail.com>
2019-05-11 15:09:32 +0300

    more tests

--- src/Bootloader/Security/RbacBootloader.php
+++ src/Bootloader/Security/RbacBootloader.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Spiral\Bootloader\Security;
 
 use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Core\Container\SingletonInterface;
 use Spiral\Security\Guard;
 use Spiral\Security\GuardInterface;
 use Spiral\Security\PermissionManager;
@@ -17,11 +18,11 @@ use Spiral\Security\PermissionsInterface;
 use Spiral\Security\RuleManager;
 use Spiral\Security\RulesInterface;
 
-final class RbacBootloader extends Bootloader
+final class RbacBootloader extends Bootloader implements SingletonInterface
 {
     const SINGLETONS = [
         PermissionsInterface::class => PermissionManager::class,
-        RulesInterface::class       => RuleManager::class,
+        RulesInterface::class       => RuleManager::class
     ];
 
     const BINDINGS = [
