ea44c2fb57e26b44497ff82acd3b8a374bcd3508
Wolfy-J <wolfy.jd@gmail.com>
2019-05-13 12:57:20 +0300

    more tests

--- tests/Framework/Module/PublishTest.php
+++ tests/Framework/Module/PublishTest.php
@@ -7,9 +7,7 @@
  */
 declare(strict_types=1);
 
-
 namespace Spiral\Framework\Module;
-
 
 use Spiral\Boot\DirectoriesInterface;
 use Spiral\Framework\ConsoleTest;
@@ -17,6 +15,13 @@ use Spiral\Framework\ConsoleTest;
 class PublishTest extends ConsoleTest
 {
     protected const TEST_FILE = __DIR__ . '/test.txt';
+
+    public function tearDown()
+    {
+        if (file_exists(self::TEST_FILE)) {
+            unlink(self::TEST_FILE);
+        }
+    }
 
     public function testPublish()
     {
@@ -69,6 +74,41 @@ class PublishTest extends ConsoleTest
         ]);
 
         $this->assertSame('original', file_get_contents($file));
+    }
+
+    /**
+     * @expectedException \Spiral\Module\Exception\PublishException
+     */
+    public function testInvalid()
+    {
+        $this->runCommandDebug('conf');
+        $file = $this->file('runtime', 'test.txt');
+        file_put_contents($file, 'original');
+        file_put_contents(self::TEST_FILE, 'test');
+
+        $this->runCommandDebug('publish', [
+            'type'   => 'invalid',
+            'target' => '@runtime/test.txt',
+            'source' => self::TEST_FILE,
+            'mode'   => 'runtime'
+        ]);
+    }
+
+    public function testReadonly()
+    {
+        $this->runCommandDebug('conf');
+        $file = $this->file('runtime', 'test.txt');
+        file_put_contents($file, 'original');
+        file_put_contents(self::TEST_FILE, 'test');
+
+        $this->runCommandDebug('publish', [
+            'type'   => 'replace',
+            'target' => '@runtime/test.txt',
+            'source' => self::TEST_FILE,
+            'mode'   => 'readonly'
+        ]);
+
+        $this->assertSame('test', file_get_contents($file));
     }
 
     public function testEnsure()
