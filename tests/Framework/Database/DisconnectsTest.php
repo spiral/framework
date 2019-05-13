2ca28ff3f6b8c1cdd86d28045853d90911235a16
Wolfy-J <wolfy.jd@gmail.com>
2019-05-13 13:47:53 +0300

    more tests

---
+++ tests/Framework/Database/DisconnectsTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Framework\Database;
+
+use Spiral\Boot\FinalizerInterface;
+use Spiral\Bootloader\Database\DisconnectsBootloader;
+use Spiral\Database\DatabaseInterface;
+use Spiral\Framework\BaseTest;
+
+class DisconnectsTest extends BaseTest
+{
+    public function testConnected()
+    {
+        $app = $this->makeApp();
+        $db = $app->get(DatabaseInterface::class);
+
+        $db->getTables();
+        $this->assertTrue($db->getDriver()->isConnected());
+    }
+
+    public function testDisconnected()
+    {
+        $app = $this->makeApp();
+        $db = $app->get(DatabaseInterface::class);
+
+        $db->getTables();
+        $this->assertTrue($db->getDriver()->isConnected());
+
+        $app->getBootloadManager()->bootload([DisconnectsBootloader::class]);
+        $this->assertTrue($db->getDriver()->isConnected());
+
+        $app->get(FinalizerInterface::class)->finalize();
+
+        $this->assertFalse($db->getDriver()->isConnected());
+    }
+}
