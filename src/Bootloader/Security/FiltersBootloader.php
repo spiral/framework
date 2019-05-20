e1315b26d50f59dca5505f4884a7b5e073db6431
Wolfy-J <wolfy.jd@gmail.com>
2019-05-20 14:26:00 +0300

    dependencies

--- src/Bootloader/Security/FiltersBootloader.php
+++ src/Bootloader/Security/FiltersBootloader.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Spiral\Bootloader\Security;
 
 use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\Bootloader\DependedInterface;
 use Spiral\Console\LocatorInterface;
 use Spiral\Filter\RequestInput;
 use Spiral\Filters\FilterLocator;
@@ -17,11 +18,21 @@ use Spiral\Filters\FilterMapper;
 use Spiral\Filters\InputInterface;
 use Spiral\Filters\MapperInterface;
 
-final class FiltersBootloader extends Bootloader
+final class FiltersBootloader extends Bootloader implements DependedInterface
 {
     const SINGLETONS = [
         MapperInterface::class  => FilterMapper::class,
         LocatorInterface::class => FilterLocator::class,
         InputInterface::class   => RequestInput::class
     ];
+
+    /**
+     * @return array
+     */
+    public function defineDependencies(): array
+    {
+        return [
+            ValidationBootloader::class
+        ];
+    }
 }
