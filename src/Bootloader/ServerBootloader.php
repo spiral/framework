c1e8feeb1bd05b7a492a98fb08d7507a104e6a30
Wolfy-J <wolfy.jd@gmail.com>
2019-07-29 14:38:26 +0300

    - automatically configure worker based on rr relay settings

--- src/Bootloader/ServerBootloader.php
+++ src/Bootloader/ServerBootloader.php
@@ -24,9 +24,9 @@ use Spiral\RoadRunner\Worker;
  */
 final class ServerBootloader extends Bootloader
 {
-    const RPC_DEFAULT = 'tcp://127.0.0.1:6001';
+    const RPC_DEFAULT    = 'tcp://127.0.0.1:6001';
     const WORKER_DEFAULT = 'pipes';
-    const SINGLETONS  = [
+    const SINGLETONS     = [
         RPC::class              => [self::class, 'rpc'],
         Worker::class           => [self::class, 'worker'],
         MetricsInterface::class => Metrics::class
@@ -67,28 +67,28 @@ final class ServerBootloader extends Bootloader
      */
     protected function worker(EnvironmentInterface $env): Worker
     {
-        $conn = $env->get('RR_WORKER', static::WORKER_DEFAULT);
+        $conn = $env->get('RR_RELAY', static::WORKER_DEFAULT);
 
         if ($conn === 'pipes' || empty($conn)) {
-            $relay = new StreamRelay(STDIN, STDOUT);
+            return new Worker(new StreamRelay(STDIN, STDOUT));
+        }
+
+        if (!preg_match('#^([a-z]+)://([^:]+):?(\d+)?$#i', $conn, $parts)) {
+            throw new BootException(
+                "Unable to configure Worker connection, invalid DSN given `{$conn}`."
+            );
+        }
+
+        if (!in_array($parts[1], ['tcp', 'unix'])) {
+            throw new BootException(
+                "Unable to configure Worker connection, invalid DSN given `{$conn}`."
+            );
+        }
+
+        if ($parts[1] == 'unix') {
+            $relay = new SocketRelay($parts[2], null, SocketRelay::SOCK_UNIX);
         } else {
-            if (!preg_match('#^([a-z]+)://([^:]+):?(\d+)?$#i', $conn, $parts)) {
-                throw new BootException(
-                    "Unable to configure Worker connection, invalid DSN given `{$conn}`."
-                );
-            }
-
-            if (!in_array($parts[1], ['tcp', 'unix'])) {
-                throw new BootException(
-                    "Unable to configure Worker connection, invalid DSN given `{$conn}`."
-                );
-            }
-
-            if ($parts[1] == 'unix') {
-                $relay = new SocketRelay($parts[2], null, SocketRelay::SOCK_UNIX);
-            } else {
-                $relay = new SocketRelay($parts[2], (int)$parts[3], SocketRelay::SOCK_TCP);
-            }
+            $relay = new SocketRelay($parts[2], (int)$parts[3], SocketRelay::SOCK_TCP);
         }
 
         return new Worker($relay);
