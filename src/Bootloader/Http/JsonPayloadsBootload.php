4d9ea0ed8fa3a64b3fa2466a1974d1fb4cef9639
Wolfy-J <wolfy.jd@gmail.com>
2019-09-16 13:27:22 +0300

    - added json payload middleware

---
+++ src/Bootloader/Http/JsonPayloadsBootload.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+declare(strict_types=1);
+
+namespace Spiral\Bootloader\Http;
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Http\Middleware\JsonPayloadMiddleware;
+
+final class JsonPayloadsBootload extends Bootloader
+{
+    const DEPENDENCIES = [
+        HttpBootloader::class
+    ];
+
+    /**
+     * @param HttpBootloader $http
+     */
+    public function boot(HttpBootloader $http)
+    {
+        $http->addMiddleware(JsonPayloadMiddleware::class);
+    }
+}
---
+++ src/Http/Middleware/JsonPayloadMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+declare(strict_types=1);
+
+namespace Spiral\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Spiral\Http\Exception\ClientException;
+
+/**
+ * Automatically parse application/json payloads.
+ */
+final class JsonPayloadMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($this->isJsonPayload($request)) {
+            try {
+                $request = $request->withParsedBody(json_decode($request->getBody()->getContents(), true));
+            } catch (\Throwable $exception) {
+                throw new ClientException(400, 'invalid json payload');
+            }
+        }
+
+        return $handler->handle($request);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return bool
+     */
+    private function isJsonPayload(ServerRequestInterface $request): bool
+    {
+        $contentType = $request->getHeaderLine('Content-Type');
+
+        return stripos($contentType, 'application/json') === 0;
+    }
+}
