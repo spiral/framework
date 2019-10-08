c0b4b9d9db7ced453e5cf10552af3ada2133b3d4
charescape <charescape@outlook.com>
2019-10-08 23:17:06 +0800

    ini_set() expects parameter 2 to be string

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@
 declare(strict_types=1);
 
 error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
+ini_set('display_errors', '1');
 
 //Composer
 require dirname(__DIR__) . '/vendor/autoload.php';
