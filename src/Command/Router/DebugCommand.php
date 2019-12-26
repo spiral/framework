0b25f51dd75b5bdf36a00b05b2412175981f0129
Aleksandr Novikov <aleksandr.novikov@spiralscout.com>
2019-12-26 18:43:23 +0300

    [feature/routes-debug-command] rewrite by wolfy-j

--- src/Command/Router/DebugCommand.php
+++ src/Command/Router/DebugCommand.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace Spiral\Command\Router;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Psr\Http\Server\RequestHandlerInterface;
 use Spiral\Console\Command;
 use Spiral\Core\Container\SingletonInterface;
+use Spiral\Router\Route;
 use Spiral\Router\RouterInterface;
 use Spiral\Router\Target\Action;
-use Spiral\Router\TargetInterface;
+use Symfony\Component\Console\Helper\Table;
 
 class DebugCommand extends Command implements SingletonInterface
 {
-    protected const NAME = 'debug:router';
+    protected const NAME = 'route:list';
     protected const DESCRIPTION = 'List application routes';
 
     /**
@@ -22,47 +21,129 @@ class DebugCommand extends Command implements SingletonInterface
      */
     public function perform(RouterInterface $router): void
     {
-        $routeParamsExtractClosure = function () {
-            return new class($this->pattern, $this->target) {
-                /** @var string $pattern */
-                public $pattern;
-                /** @var string|callable|RequestHandlerInterface|TargetInterface $target */
-                public $target;
+        $grid = $this->table(['Verbs:', 'Pattern:', 'Target:']);
 
-                public function __construct($pattern, $target)
-                {
-                    $this->target = $target;
-                    $this->pattern = $pattern;
-                }
-            };
-        };
-
-        $closureCallable = function (string $method, array $arguments) {
-            return $this->{$method}(...$arguments);
-        };
-
-        $rows = new ArrayCollection();
-        foreach ($this->getRoutes($router) as $route) {
-            $extractedParams = $routeParamsExtractClosure->call($route);
-            if ($extractedParams->target instanceof Action) {
-                $closureCallable->bindTo($extractedParams->target, Action::class);
-                $handler = $closureCallable->call($extractedParams->target, 'resolveController', [[]])
-                    . '::' .
-                    $closureCallable->call($extractedParams->target, 'resolveAction', [[]]);
-            } elseif (is_object($extractedParams->target)) {
-                $handler = get_class($extractedParams->target);
-            } else {
-                $handler = $extractedParams->target;
+        foreach ($this->getRoutes($router) as $name => $route) {
+            if ($route instanceof Route) {
+                $this->renderRoute($grid, $name, $route);
             }
-            $rows->add(
-                [$extractedParams->pattern, implode(',', $route->getVerbs()), $handler]
-            );
         }
-        $this->table(['Pattern', 'Verbs', 'Action'], $rows->toArray())->render();
+
+        $grid->render();
     }
 
-    public function getRoutes(RouterInterface $router): ?\Generator
+    /**
+     * @param Table $table
+     * @param string $name
+     * @param Route $route
+     */
+    private function renderRoute(Table $table, string $name, Route $route)
+    {
+        $table->addRow(
+            [
+                $this->getVerbs($route),
+                $this->getPattern($route),
+                $this->getTarget($route)
+            ]
+        );
+    }
+
+    /**
+     * @param RouterInterface $router
+     * @return \Generator|null
+     */
+    private function getRoutes(RouterInterface $router): ?\Generator
     {
         yield from $router->getRoutes();
+    }
+
+    /**
+     * @param Route $route
+     * @return string
+     */
+    private function getVerbs(Route $route): string
+    {
+        $result = [];
+
+        foreach ($route->getVerbs() as $verb) {
+            switch (strtolower($verb)) {
+                case 'get':
+                    $verb = '<fg=green>GET</>';
+                    break;
+                case 'post':
+                    $verb = '<fg=blue>POST</>';
+                    break;
+                case 'put':
+                    $verb = '<fg=yellow>PUT</>';
+                    break;
+                case 'delete':
+                    $verb = '<fg=red>DELETE</>';
+                    break;
+            }
+
+            $result[] = $verb;
+        }
+
+        return implode(', ', $result);
+    }
+
+    /**
+     * @param Route $route
+     * @return string
+     */
+    private function getPattern(Route $route): string
+    {
+        $pattern = $this->getValue($route->getUriHandler(), 'pattern');
+        $pattern = str_replace(
+            '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}',
+            'uuid',
+            $pattern
+        );
+
+        return preg_replace_callback(
+            '/<([^>]*)>/',
+            static function ($m) {
+                return sprintf('<fg=yellow>%s</>', $m[0]);
+            },
+            $pattern
+        );
+    }
+
+    /**
+     * @param Route $route
+     * @return string
+     */
+    private function getTarget(Route $route): string
+    {
+        $target = $this->getValue($route, 'target');
+        switch (true) {
+            case $target instanceof Action:
+                return sprintf(
+                    '%s::%s',
+                    $this->getValue($target, 'controller'),
+                    $this->getValue($target, 'action')
+                );
+        }
+
+        return '';
+        return $this->getValue($route->getUriHandler(), 'pattern');
+    }
+
+    /**
+     * @param object $object
+     * @param string $property
+     * @return mixed
+     */
+    private function getValue(object $object, string $property)
+    {
+        try {
+            $r = new \ReflectionObject($object);
+            $prop = $r->getProperty($property);
+            $prop->setAccessible(true);
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+
+        return $prop->getValue($object);
     }
 }
