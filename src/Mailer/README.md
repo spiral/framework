7caf51032f6fbcf47ff3930e7d118432c0a458e7
SerafimArts <nesk@xakep.ru>
2020-09-19 06:42:56 +0300

    Merge mailer component

---
+++ src/Mailer/README.md
@@ -0,0 +1,9 @@
+Mailing Interfaces
+========
+[![Latest Stable Version](https://poser.pugx.org/spiral/mailer/v/stable)](https://packagist.org/packages/spiral/mailer) 
+[![Build Status](https://github.com/spiral/mailer/workflows/build/badge.svg)](https://github.com/spiral/mailer/actions)
+[![Codecov](https://codecov.io/gh/spiral/mailer/branch/master/graph/badge.svg)](https://codecov.io/gh/spiral/mailer/)
+
+License:
+--------
+MIT License (MIT). Please see [`LICENSE`](./LICENSE) for more information. Maintained by [Spiral Scout](https://spiralscout.com).
---
+++ src/Mailer/tests/MessageTest.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Mailer;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Mailer\Message;
+
+class MessageTest extends TestCase
+{
+    public function testDefaults(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+        $this->assertSame('test', $m->getSubject());
+        $this->assertSame(['email@domain.com'], $m->getTo());
+        $this->assertSame([], $m->getData());
+        $this->assertSame([], $m->getCC());
+        $this->assertSame([], $m->getBCC());
+        $this->assertNull($m->getFrom());
+        $this->assertNull($m->getReplyTo());
+    }
+
+    public function testData(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+        $this->assertSame([], $m->getData());
+
+        $m->setData(['hello' => 'world']);
+        $this->assertSame(['hello' => 'world'], $m->getData());
+    }
+
+    public function testOptions(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+        $this->assertSame([], $m->getOptions());
+
+        $m->setOptions(['hello' => 'world']);
+        $this->assertSame(['hello' => 'world'], $m->getOptions());
+
+        $m->setOption('k', 'v');
+        $this->assertSame(['hello' => 'world', 'k' => 'v'], $m->getOptions());
+    }
+
+    public function testTo(): void
+    {
+        $m = new Message('test', ['email@domain.com', 'email2@domain.com']);
+        $this->assertSame(['email@domain.com', 'email2@domain.com'], $m->getTo());
+
+        $m->setTo('email@domain.com');
+        $this->assertSame(['email@domain.com'], $m->getTo());
+
+        $m->setTo('email@domain.com', 'another@domain.com');
+        $this->assertSame(['email@domain.com', 'another@domain.com'], $m->getTo());
+
+        $m->setTo();
+        $this->assertSame([], $m->getTo());
+    }
+
+    public function testCC(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+
+        $this->assertSame([], $m->getCC());
+        $m->setCC('email@domain.com');
+        $this->assertSame(['email@domain.com'], $m->getCC());
+
+        $m->setCC('email@domain.com', 'another@domain.com');
+        $this->assertSame(['email@domain.com', 'another@domain.com'], $m->getCC());
+
+        $m->setCC();
+        $this->assertSame([], $m->getCC());
+    }
+
+    public function testBCC(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+
+        $this->assertSame([], $m->getBCC());
+        $m->setBCC('email@domain.com');
+        $this->assertSame(['email@domain.com'], $m->getBCC());
+
+        $m->setBCC('email@domain.com', 'another@domain.com');
+        $this->assertSame(['email@domain.com', 'another@domain.com'], $m->getBCC());
+
+        $m->setBCC();
+        $this->assertSame([], $m->getBCC());
+    }
+
+    public function testFrom(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+
+        $this->assertNull($m->getFrom());
+
+        $m->setFrom('email@domain.com');
+        $this->assertSame('email@domain.com', $m->getFrom());
+
+        $m->setFrom(null);
+        $this->assertNull($m->getFrom());
+    }
+
+    public function testReplyTo(): void
+    {
+        $m = new Message('test', 'email@domain.com');
+
+        $this->assertNull($m->getReplyTo());
+
+        $m->setReplyTo('email@domain.com');
+        $this->assertSame('email@domain.com', $m->getReplyTo());
+
+        $m->setReplyTo(null);
+        $this->assertNull($m->getReplyTo());
+    }
+}
