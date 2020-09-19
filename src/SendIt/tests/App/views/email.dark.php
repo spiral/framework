40de7d44683d6d39b6ca25d5588d2dcf9cca903b
SerafimArts <nesk@xakep.ru>
2020-09-19 07:33:59 +0300

    Fix codestyle CI bug (removing trailing "?>" tag in views)

--- src/SendIt/tests/App/views/email.dark.php
+++ src/SendIt/tests/App/views/email.dark.php
@@ -1,7 +1,7 @@
 <extends:sendit:builder subject="Demo Email"/>
 <use:bundle path="sendit:bundle"/>
 
-<email:attach path="{{ directory('config') . 'mailer.php'}}" name="bootstrap.txt" />
+<email:attach path="{{ directory('root') . 'example.txt' }}" name="bootstrap.txt" />
 
 <block:html>
     <p>Hello, {{ $name }}!</p>
