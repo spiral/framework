03a3cdaba792ef419107264dc0181a49140347ff
SerafimArts <nesk@xakep.ru>
2020-09-29 14:30:31 +0300

    Fix default "secure" flag in auth bootloader

--- src/Framework/Bootloader/Auth/HttpAuthBootloader.php
+++ src/Framework/Bootloader/Auth/HttpAuthBootloader.php
@@ -62,18 +62,23 @@ final class HttpAuthBootloader extends Bootloader implements SingletonInterface
             [
                 'defaultTransport' => 'cookie',
                 'transports'       => [
-                    'cookie' => new CookieTransport(
-                        'token',
-                        $this->config->getConfig('http')['basePath'],
-                        null,
-                        true,
-                        true,
-                        null
-                    ),
+                    'cookie' => $this->createDefaultCookieTransport(),
                     'header' => new HeaderTransport('X-Auth-Token'),
                 ],
             ]
         );
+    }
+
+    /**
+     * Creates default cookie transport when "transports" section is empty.
+     *
+     * @return CookieTransport
+     */
+    private function createDefaultCookieTransport(): CookieTransport
+    {
+        $config = $this->config->getConfig('http');
+
+        return new CookieTransport('token', $config['basePath'] ?? '/');
     }
 
     /**
