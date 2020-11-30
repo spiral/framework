a9bef211813315d6b7c355ec86d7c72717d86c55
Valentin V / vvval <valentin@spiralscout.com>
2020-11-30 16:34:33 +0300

    refactor NamedArgumentsInstantiator.php

--- src/Attributes/src/Internal/Instantiator/NamedArgumentsInstantiator.php
+++ src/Attributes/src/Internal/Instantiator/NamedArgumentsInstantiator.php
@@ -14,7 +14,7 @@ namespace Spiral\Attributes\Internal\Instantiator;
 use Spiral\Attributes\Internal\Exception;
 
 /**
- * @internal NamedArgumentsInstantiator is an internal library class, please do not use it in your code.
+ * @internal       NamedArgumentsInstantiator is an internal library class, please do not use it in your code.
  * @psalm-internal Spiral\Attributes
  */
 final class NamedArgumentsInstantiator extends Instantiator
@@ -31,8 +31,8 @@ final class NamedArgumentsInstantiator extends Instantiator
 
     /**
      * @param \ReflectionClass $attr
-     * @param array $arguments
-     * @param string $context
+     * @param array            $arguments
+     * @param string           $context
      * @return object
      * @throws \Throwable
      */
@@ -66,9 +66,9 @@ final class NamedArgumentsInstantiator extends Instantiator
     }
 
     /**
-     * @param \ReflectionClass $ctx
+     * @param \ReflectionClass  $ctx
      * @param \ReflectionMethod $constructor
-     * @param array $arguments
+     * @param array             $arguments
      * @return array
      * @throws \Throwable
      */
@@ -93,9 +93,9 @@ final class NamedArgumentsInstantiator extends Instantiator
     }
 
     /**
-     * @param \ReflectionClass $ctx
+     * @param \ReflectionClass     $ctx
      * @param \ReflectionParameter $param
-     * @param array $arguments
+     * @param array                $arguments
      * @return mixed
      * @throws \Throwable
      */
@@ -103,18 +103,14 @@ final class NamedArgumentsInstantiator extends Instantiator
     {
         switch (true) {
             case \array_key_exists($param->getName(), $arguments):
-                try {
-                    return $arguments[$param->getName()];
-                } finally {
-                    unset($arguments[$param->getName()]);
-                }
+                $argument =  $arguments[$param->getName()];
+                unset($arguments[$param->getName()]);
+                return $argument;
 
             case \array_key_exists($param->getPosition(), $arguments):
-                try {
-                    return $arguments[$param->getPosition()];
-                } finally {
-                    unset($arguments[$param->getPosition()]);
-                }
+                $argument =  $arguments[$param->getPosition()];
+                unset($arguments[$param->getPosition()]);
+                return $argument;
 
             case $param->isDefaultValueAvailable():
                 return $param->getDefaultValue();
