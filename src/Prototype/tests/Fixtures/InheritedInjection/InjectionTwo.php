77e7fdd8f995518817db3fcd0f6d0cf3b38910d2
Valentin V / vvval <valentin@spiralscout.com>
2020-12-14 20:34:43 +0300

    clean fixtures

--- src/Prototype/tests/Fixtures/InheritedInjection/InjectionTwo.php
+++ src/Prototype/tests/Fixtures/InheritedInjection/InjectionTwo.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Prototype\Fixtures\InheritedInjection;
 
-use Spiral\Prototype\Annotation\Prototyped;
-
-/**
-* @Prototyped(property="two")
-*/
 class InjectionTwo
 {
 }
