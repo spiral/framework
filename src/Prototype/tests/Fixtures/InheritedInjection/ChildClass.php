3fbbd8ccf93d256801da4a09f52003970df3b036
Valentin V / vvval <valentin@spiralscout.com>
2020-12-15 17:47:56 +0300

    clean fixture

--- src/Prototype/tests/Fixtures/InheritedInjection/ChildClass.php
+++ src/Prototype/tests/Fixtures/InheritedInjection/ChildClass.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Prototype\Fixtures\InheritedInjection;
 
-use Spiral\Prototype\Traits\PrototypeTrait;
-
 class ChildClass extends MiddleClass
 {
-    use PrototypeTrait;
-
     public function useTwo(): void
     {
         $this->two;
