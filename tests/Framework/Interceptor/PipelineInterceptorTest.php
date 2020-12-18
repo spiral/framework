4d4d5c9ef585f29ceebe285300c99aea88d4cb7e
Valentin V / vvval <valentin@spiralscout.com>
2020-12-18 18:03:24 +0300

    clean tests

--- tests/Framework/Interceptor/PipelineInterceptorTest.php
+++ tests/Framework/Interceptor/PipelineInterceptorTest.php
@@ -11,7 +11,7 @@ class PipelineInterceptorTest extends HttpTest
     public function testWithoutPipeline(): void
     {
         $response = $this->get('/intercepted/without')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         //appends are executed after the sub-core action called: one->two->three->action[without]->[three]->[two]->[one]
         $this->assertSame(['without', 'three', 'two', 'one'], $output);
     }
@@ -19,14 +19,14 @@ class PipelineInterceptorTest extends HttpTest
     public function testWith(): void
     {
         $response = $this->get('/intercepted/with')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         $this->assertSame(['with', 'three', 'two', 'one'], $output);
     }
 
     public function testMix(): void
     {
         $response = $this->get('/intercepted/mix')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         //pipeline interceptors are injected into the middle
         $this->assertSame(['mix', 'six', 'three', 'two', 'one', 'five', 'four'], $output);
     }
@@ -34,7 +34,7 @@ class PipelineInterceptorTest extends HttpTest
     public function testDup(): void
     {
         $response = $this->get('/intercepted/dup')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         //pipeline interceptors are added to the end
         $this->assertSame(['dup', 'three', 'two', 'one', 'three', 'two', 'one'], $output);
     }
@@ -42,7 +42,7 @@ class PipelineInterceptorTest extends HttpTest
     public function testSkipNext(): void
     {
         $response = $this->get('/intercepted/skip')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         //interceptors after current pipeline are ignored
         $this->assertSame(['skip', 'three', 'two', 'one', 'one'], $output);
     }
@@ -50,7 +50,7 @@ class PipelineInterceptorTest extends HttpTest
     public function testSkipIfFirst(): void
     {
         $response = $this->get('/intercepted/first')->getBody();
-        $output = json_decode((string)$response, true, 512, JSON_THROW_ON_ERROR);
+        $output = json_decode((string)$response, true);
         //interceptors after current pipeline are ignored
         $this->assertSame(['first', 'three', 'two', 'one'], $output);
     }
