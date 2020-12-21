f93b3eea86118f0f8dbbed518b535942d9922502
SerafimArts <nesk@xakep.ru>
2020-12-21 14:43:15 +0300

    Remove dev packages

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -100,7 +100,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES, [
         'src/AnnotatedRoutes' => 'git@github.com:spiral/annotated-routes.git',
         'src/Annotations'     => 'git@github.com:spiral/annotations.git',
-        'src/Attributes'      => 'git@github.com:spiral/attributes.git',
         'src/Auth'            => 'git@github.com:spiral/auth.git',
         'src/AuthHttp'        => 'git@github.com:spiral/auth-http.git',
         'src/Boot'            => 'git@github.com:spiral/boot.git',
--- src/Attributes/resources/.phpstorm.meta.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace PHPSTORM_META {
-    
-}
