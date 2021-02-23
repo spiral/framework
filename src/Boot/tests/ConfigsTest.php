2f27e520345cc264039dbabfecc1f53de42030cb
Valentin V <vvval@users.noreply.github.com>
2021-02-23 10:58:40 +0300

    Update src/Boot/tests/ConfigsTest.php

--- src/Boot/tests/ConfigsTest.php
+++ src/Boot/tests/ConfigsTest.php
@@ -32,7 +32,7 @@ class ConfigsTest extends TestCase
         $this->assertSame(['key' => 'value'], $config->toArray());
     }
 
-    public function testDep(): void
+    public function testCustomConfigLoader(): void
     {
         $core = TestCore::init([
             'root'   => __DIR__,
