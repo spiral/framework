1f074f1434938b9d8c5a5de489da1692401a62ef
Vladislav Gorenkin <gorenkin@ruvents.com>
2021-06-10 21:41:28 +0600

    Allow to pass 'options' array for s3 storage adapter
    (cherry picked from commit 0fd416350c6a76a2e5d73707f7149feb6003fbee)

--- src/Framework/Bootloader/Storage/StorageConfig.php
+++ src/Framework/Bootloader/Storage/StorageConfig.php
@@ -201,7 +201,7 @@ class StorageConfig
                 $server['token'] ?? null,
                 $server['expires'] ?? null
             ),
-        ];
+        ] + ($server['options'] ?? []);
 
         $name = $bucket['bucket'] ?? $server['bucket'];
         $visibility = $bucket['visibility'] ?? $server['bucket'] ?? Visibility::VISIBILITY_PUBLIC;
