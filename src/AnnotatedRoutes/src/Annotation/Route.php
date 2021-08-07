932448d488f35265be24fe2e99e64debd8a39c0d
Vladislav Gorenkin <gorenkin@ruvents.com>
2021-08-07 12:19:04 +0600

    Define routes with attributes

--- src/AnnotatedRoutes/src/Annotation/Route.php
+++ src/AnnotatedRoutes/src/Annotation/Route.php
@@ -11,61 +11,87 @@ declare(strict_types=1);
 
 namespace Spiral\Router\Annotation;
 
-use Doctrine\Common\Annotations\Annotation\Target;
+use Attribute;
+use Doctrine\Common\Annotations\Annotation;
+use Spiral\Attributes\NamedArgumentConstructor;
 
 /**
- * @Annotation
- * @Target({"METHOD"})
+ * @Annotation()
+ * @Annotation\Target({"METHOD"})
+ * @NamedArgumentConstructor()
  */
+#[Attribute(Attribute::TARGET_METHOD)]
+#[NamedArgumentConstructor()]
 final class Route
 {
     public const DEFAULT_GROUP = 'default';
 
     /**
-     * @Attribute(name="route", type="string", required=true)
+     * @Annotation\Attribute(name="route", type="string", required=true)
      * @var string
      */
     public $route;
 
     /**
-     * @Attribute(name="name", type="string")
+     * @Annotation\Attribute(name="name", type="string", required=true)
      * @var string
      */
     public $name = null;
 
     /**
-     * @Attribute(name="verbs", type="mixed", required=true)
+     * @Annotation\Attribute(name="verbs", type="mixed", required=true)
      * @var mixed
      */
-    public $methods = \Spiral\Router\Route::VERBS;
+    public $methods;
 
     /**
      * Default match options.
      *
-     * @Attribute(name="defaults", type="array")
+     * @Annotation\Attribute(name="defaults", type="array")
      * @var array
      */
-    public $defaults = [];
+    public $defaults;
 
     /**
      * Route group (set of middlewere), groups can be configured using MiddlewareRegistry.
      *
-     * @Attribute(name="group", type="string")
-     * @var string
+     * @Annotation\Attribute(name="group", type="string")
+     * @var null|string
      */
-    public $group = self::DEFAULT_GROUP;
+    public $group;
 
     /**
      * Route specific middleware set, if any.
      *
-     * @Attribute(name="middleware", type="array")
+     * @Annotation\Attribute(name="middleware", type="array")
      * @var array
      */
-    public $middleware = [];
+    public $middleware;
 
     /**
-     * @Attribute(name="priority", type="int")
+     * @Annotation\Attribute(name="priority", type="int")
      * @var int
      */
-    public $priority = 0;
+    public $priority;
+
+    /**
+     * @param array|string $methods
+     */
+    public function __construct(
+        string $route,
+        string $name,
+        $methods = \Spiral\Router\Route::VERBS,
+        array $defaults = [],
+        string $group = self::DEFAULT_GROUP,
+        array $middleware = [],
+        int $priority = 0
+    ) {
+        $this->route = $route;
+        $this->name = $name;
+        $this->methods = $methods;
+        $this->defaults = $defaults;
+        $this->group = $group;
+        $this->middleware = $middleware;
+        $this->priority = $priority;
+    }
 }
