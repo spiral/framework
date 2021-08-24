c8856091f5ce089b1d8b5a15fe947357b67b1f38
SerafimArts <nesk@xakep.ru>
2021-08-24 17:51:56 +0300

    Add S3 and Cloudfront prefixes support

--- src/Distribution/src/Resolver/UriResolver.php
+++ src/Distribution/src/Resolver/UriResolver.php
@@ -15,4 +15,17 @@ use Spiral\Distribution\UriResolverInterface;
 
 abstract class UriResolver implements UriResolverInterface
 {
+    /**
+     * @param string $file
+     * @param string|null $prefix
+     * @return string
+     */
+    protected function concat(string $file, ?string $prefix): string
+    {
+        if ($prefix === null) {
+            return $file;
+        }
+
+        return \trim($prefix, '/') . '/' . \trim($file, '/');
+    }
 }
