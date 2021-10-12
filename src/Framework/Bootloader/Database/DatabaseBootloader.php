5f8c24d33e481c5d0f2f9c66e08e6104b0fa1f06
SerafimArts <nesk@xakep.ru>
2021-10-12 16:07:14 +0300

    Add support of native cycle/database and cycle/migrations packages

--- src/Framework/Bootloader/Database/DatabaseBootloader.php
+++ src/Framework/Bootloader/Database/DatabaseBootloader.php
@@ -13,22 +13,19 @@ namespace Spiral\Bootloader\Database;
 
 use Spiral\Boot\Bootloader\Bootloader;
 use Spiral\Config\ConfiguratorInterface;
+use Spiral\Core\Container;
 use Spiral\Core\Container\SingletonInterface;
-use Spiral\Database\Database;
-use Spiral\Database\DatabaseInterface;
-use Spiral\Database\DatabaseManager;
-use Spiral\Database\DatabaseProviderInterface;
+use Spiral\Database\Database as SpiralDatabase;
+use Cycle\Database\Database as CycleDatabase;
+use Spiral\Database\DatabaseInterface as SpiralDatabaseInterface;
+use Cycle\Database\DatabaseInterface as CycleDatabaseInterface;
+use Spiral\Database\DatabaseManager as SpiralDatabaseManager;
+use Cycle\Database\DatabaseManager as CycleDatabaseManager;
+use Spiral\Database\DatabaseProviderInterface as SpiralDatabaseProviderInterface;
+use Cycle\Database\DatabaseProviderInterface as CycleDatabaseProviderInterface;
 
 final class DatabaseBootloader extends Bootloader implements SingletonInterface
 {
-    protected const SINGLETONS = [
-        DatabaseProviderInterface::class => DatabaseManager::class,
-    ];
-
-    protected const BINDINGS = [
-        DatabaseInterface::class => Database::class,
-    ];
-
     /** @var ConfiguratorInterface */
     private $config;
 
@@ -42,17 +39,51 @@ final class DatabaseBootloader extends Bootloader implements SingletonInterface
 
     /**
      * Init database config.
+     *
+     * @param Container $container
      */
-    public function boot(): void
+    public function boot(Container $container): void
     {
-        $this->config->setDefaults(
-            'database',
-            [
-                'default'   => 'default',
-                'aliases'   => [],
-                'databases' => [],
-                'drivers'   => [],
-            ]
-        );
+        $this->bootConfigs();
+
+        $container->bindSingleton(SpiralDatabaseProviderInterface::class, SpiralDatabaseManager::class);
+        $container->bind(SpiralDatabaseInterface::class, SpiralDatabase::class);
+
+        if (\class_exists(CycleDatabaseManager::class)) {
+            $container->bindSingleton(CycleDatabaseProviderInterface::class,
+                static function (SpiralDatabaseProviderInterface $manager) {
+                    return $manager;
+                });
+
+            $container->bindSingleton(CycleDatabaseManager::class,
+                static function (SpiralDatabaseManager $manager) {
+                    return $manager;
+                });
+        }
+
+        if (\class_exists(CycleDatabase::class)) {
+            $container->bind(CycleDatabaseInterface::class,
+                static function (SpiralDatabaseInterface $database) {
+                    return $database;
+                });
+
+            $container->bind(CycleDatabase::class,
+                static function (SpiralDatabaseInterface $database) {
+                    return $database;
+                });
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function bootConfigs(): void
+    {
+        $this->config->setDefaults('database', [
+            'default'   => 'default',
+            'aliases'   => [],
+            'databases' => [],
+            'drivers'   => [],
+        ]);
     }
 }
--- src/Framework/Bootloader/Database/MigrationsBootloader.php
+++ src/Framework/Bootloader/Database/MigrationsBootloader.php
@@ -16,9 +16,15 @@ use Spiral\Boot\DirectoriesInterface;
 use Spiral\Boot\EnvironmentInterface;
 use Spiral\Bootloader\TokenizerBootloader;
 use Spiral\Config\ConfiguratorInterface;
+use Spiral\Core\Container;
 use Spiral\Migrations\FileRepository;
+use Cycle\Migrations\FileRepository as CycleFileRepository;
 use Spiral\Migrations\Migrator;
+use Cycle\Migrations\Migrator as CycleMigrator;
+use Spiral\Migrations\MigratorInterface;
+use Cycle\Migrations\MigratorInterface as CycleMigratorInterface;
 use Spiral\Migrations\RepositoryInterface;
+use Cycle\Migrations\RepositoryInterface as CycleRepositoryInterface;
 
 final class MigrationsBootloader extends Bootloader
 {
@@ -27,32 +33,72 @@ final class MigrationsBootloader extends Bootloader
         DatabaseBootloader::class,
     ];
 
-    protected const SINGLETONS = [
-        Migrator::class            => Migrator::class,
-        RepositoryInterface::class => FileRepository::class,
-    ];
+    /**
+     * @var ConfiguratorInterface
+     */
+    private $config;
 
     /**
      * @param ConfiguratorInterface $config
-     * @param EnvironmentInterface  $env
-     * @param DirectoriesInterface  $dirs
      */
-    public function boot(
-        ConfiguratorInterface $config,
-        EnvironmentInterface $env,
-        DirectoriesInterface $dirs
-    ): void {
+    public function __construct(ConfiguratorInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @param EnvironmentInterface $env
+     * @param DirectoriesInterface $dirs
+     * @param Container $container
+     * @return void
+     */
+    public function boot(EnvironmentInterface $env, DirectoriesInterface $dirs, Container $container): void
+    {
         if (!$dirs->has('migrations')) {
             $dirs->set('migrations', $dirs->get('app') . 'migrations');
         }
 
-        $config->setDefaults(
-            'migration',
-            [
-                'directory' => $dirs->get('migrations'),
-                'table'     => 'migrations',
-                'safe'      => $env->get('SAFE_MIGRATIONS', false),
-            ]
-        );
+        $this->bootConfigs($env, $dirs);
+
+        $container->bindSingleton(MigratorInterface::class, Migrator::class);
+        $container->bindSingleton(RepositoryInterface::class, FileRepository::class);
+
+        if (\class_exists(CycleMigrator::class)) {
+            $container->bindSingleton(CycleMigratorInterface::class,
+                static function (MigratorInterface $migrator) {
+                    return $migrator;
+                });
+
+            $container->bindSingleton(CycleMigrator::class,
+                static function (Migrator $migrator) {
+                    return $migrator;
+                });
+        }
+
+        if (\class_exists(CycleFileRepository::class)) {
+            $container->bindSingleton(CycleFileRepository::class,
+                static function (FileRepository $repository) {
+                    return $repository;
+                });
+
+            $container->bindSingleton(CycleRepositoryInterface::class,
+                static function (RepositoryInterface $repository) {
+                    return $repository;
+                });
+        }
+    }
+
+    /**
+     * @param EnvironmentInterface $env
+     * @param DirectoriesInterface $dirs
+     * @return void
+     */
+    private function bootConfigs(EnvironmentInterface $env, DirectoriesInterface $dirs): void
+    {
+        $this->config->setDefaults('migration', [
+            'directory' => $dirs->get('migrations'),
+            'table'     => 'migrations',
+            'safe'      => $env->get('SAFE_MIGRATIONS', false),
+        ]);
     }
 }
