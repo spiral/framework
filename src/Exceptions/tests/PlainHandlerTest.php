f2c6729f4a6803aaf63d238cfc3c3517458f9f0d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-09 18:10:53 +0700

    add test for missing space for PlainHandler

---
+++ src/Exceptions/tests/PlainHandlerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Exceptions;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Spiral\Exceptions\PlainHandler;
+
+class PlainHandlerTest extends TestCase
+{
+    public function testRenderException(): void
+    {
+        $plainHandler = new PlainHandler();
+        $result = $plainHandler->renderException(new Exception('Undefined variable $undefined'));
+
+        $this->assertStringContainsString(
+            sprintf('Undefined variable $undefined in %s:24', __FILE__),
+            $result
+        );
+    }
+}
