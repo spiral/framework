6d7d2cd4e07508437a67340475bf38247bcc90d3
Maxim Smakouz <m.smakouz@gmail.com>
2022-02-01 15:39:21 +0200

    Add ability to configure Monolog processors

---
+++ src/Bridge/Monolog/tests/ProcessorsTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Monolog;
+
+use Monolog\Processor\PsrLogMessageProcessor;
+use PHPUnit\Framework\TestCase;
+use Spiral\Boot\BootloadManager;
+use Spiral\Config\ConfigManager;
+use Spiral\Config\ConfiguratorInterface;
+use Spiral\Config\LoaderInterface;
+use Spiral\Core\Container;
+use Spiral\Logger\ListenerRegistry;
+use Spiral\Logger\ListenerRegistryInterface;
+use Spiral\Logger\LogsInterface;
+use Spiral\Monolog\Bootloader\MonologBootloader;
+use Spiral\Monolog\Config\MonologConfig;
+use Spiral\Monolog\Exception\ConfigException;
+
+class ProcessorsTest extends TestCase
+{
+    /** @var Container */
+    private $container;
+
+    public function setUp(): void
+    {
+        $this->container = new Container();
+        $this->container->bind(ConfiguratorInterface::class, new ConfigManager(
+            new class() implements LoaderInterface {
+                public function has(string $section): bool
+                {
+                    return false;
+                }
+
+                public function load(string $section): array
+                {
+                    return [];
+                }
+            }
+        ));
+        $this->container->bindSingleton(ListenerRegistryInterface::class, new ListenerRegistry());
+        $this->container->get(BootloadManager::class)->bootload([MonologBootloader::class]);
+    }
+
+    public function testDefaultProcessor(): void
+    {
+        $this->container->bind(MonologConfig::class, new MonologConfig());
+
+        $logger = $this->container->get(LogsInterface::class)->getLogger('test');
+        $this->assertSame('test', $logger->getName());
+        $this->assertCount(1, $logger->getProcessors());
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $logger->getProcessors()[0]);
+    }
+
+    public function testInvalidProcessor(): void
+    {
+        $this->expectException(ConfigException::class);
+
+        $this->container->bind(MonologConfig::class, new MonologConfig([
+            'processors' => [
+                'test' => [
+                    ['what?']
+                ]
+            ]
+        ]));
+
+        $this->container->get(LogsInterface::class)->getLogger('test');
+    }
+
+    public function testObjectProcessor(): void
+    {
+        $this->container->bind(MonologConfig::class, new MonologConfig([
+            'processors' => [
+                'test' => [
+                    new PsrLogMessageProcessor()
+                ]
+            ]
+        ]));
+
+        $logger = $this->container->get(LogsInterface::class)->getLogger('test');;
+
+        $this->assertCount(1, $logger->getProcessors());
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $logger->getProcessors()[0]);
+    }
+
+    public function testBindedProcessor(): void
+    {
+        $this->container->bind('PsrLogMessageProcessor', new PsrLogMessageProcessor());
+        $this->container->bind(MonologConfig::class, new MonologConfig([
+            'processors' => [
+                'test' => [
+                    'PsrLogMessageProcessor'
+                ]
+            ]
+        ]));
+
+        $logger = $this->container->get(LogsInterface::class)->getLogger('test');
+
+        $this->assertCount(1, $logger->getProcessors());
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $logger->getProcessors()[0]);
+        $this->assertSame($this->container->get('PsrLogMessageProcessor'), $logger->getProcessors()[0]);
+    }
+
+    public function testConstructProcessor(): void
+    {
+        $this->container->bind(MonologConfig::class, new MonologConfig([
+            'processors' => [
+                'test' => [
+                    [
+                        'class' => PsrLogMessageProcessor::class
+                    ]
+                ]
+            ]
+        ]));
+
+        $logger = $this->container->get(LogsInterface::class)->getLogger('test');
+
+        $this->assertCount(1, $logger->getProcessors());
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $logger->getProcessors()[0]);
+    }
+
+    public function testConstructWithOptionsProcessor(): void
+    {
+        $this->container->bind(MonologConfig::class, new MonologConfig([
+            'processors' => [
+                'test' => [
+                    [
+                        'class'   => PsrLogMessageProcessor::class,
+                        'options' => [
+                            'dateFormat' => 'c'
+                        ]
+                    ]
+                ]
+            ]
+        ]));
+
+        $logger = $this->container->get(LogsInterface::class)->getLogger('test');
+        $processor = $logger->getProcessors()[0];
+
+        $this->assertCount(1, $logger->getProcessors());
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $processor);
+
+        $property = new \ReflectionProperty(PsrLogMessageProcessor::class, 'dateFormat');
+        $property->setAccessible(true);
+
+        $this->assertSame('c', $property->getValue($processor));
+    }
+}
