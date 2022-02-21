ef7ca88b3fcf5e03a0707b0dc85d5c50ecf385ce
butschster <butschster@gmail.com>
2022-02-21 13:39:33 +0300

    Add ability to use null driver for queue connections
    fixed #569

---
+++ src/Queue/src/Driver/NullDriver.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Queue\Driver;
+
+use Ramsey\Uuid\Uuid;
+use Spiral\Queue\OptionsInterface;
+use Spiral\Queue\QueueInterface;
+use Spiral\Queue\QueueTrait;
+
+final class NullDriver implements QueueInterface
+{
+    use QueueTrait;
+
+    public function push(string $name, array $payload = [], OptionsInterface $options = null): string
+    {
+        // Do nothing
+
+        return (string) Uuid::uuid4();
+    }
+}
