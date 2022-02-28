cfb0e4a45744fcaa3e8f0b35fb4f7632b2460da6
roxblnfk <roxblnfk@ya.ru>
2022-02-28 15:28:23 +0300

    Fix monorepo-builder.php

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -100,7 +100,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             'cycle/migrations'          => '^1.0.1',
             'cycle/proxy-factory'       => '^1.2',
             'cycle/schema-builder'      => '^1.1',
-            'symplify/monorepo-builder' => '^8.3',
+            'symplify/monorepo-builder' => '^10.0',
             'vimeo/psalm'               => '^4.3',
         ],
     ]);
