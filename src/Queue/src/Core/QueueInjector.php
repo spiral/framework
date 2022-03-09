38fc45339a119e6f99858ed6ba1c61cbac6e6533
roxblnfk <roxblnfk@ya.ru>
2022-03-10 00:11:55 +0300

    Add injector

---
+++ src/Queue/src/Core/QueueInjector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Queue\Core;
+
+use ReflectionClass;
+use Spiral\Core\Container\InjectorInterface;
+use Spiral\Queue\Exception\InvalidArgumentException;
+use Spiral\Queue\QueueConnectionProviderInterface;
+use Spiral\Queue\QueueInterface;
+
+class QueueInjector implements InjectorInterface
+{
+    private QueueConnectionProviderInterface $queueManager;
+
+    public function __construct(QueueConnectionProviderInterface $queueManager)
+    {
+        $this->queueManager = $queueManager;
+    }
+
+    public function createInjection(ReflectionClass $class, string $context = null): QueueInterface
+    {
+        // Get Queue by context
+        if ($context === null) {
+            $connection = $this->queueManager->getConnection();
+        } else {
+            try {
+                $connection = $this->queueManager->getConnection($context);
+            } catch (InvalidArgumentException $e) {
+                // Case when context doesn't match to configured connections
+                return $this->queueManager->getConnection();
+            }
+        }
+
+        // User specified a specific class type
+        $className = $class->getName();
+        if ($className !== QueueInterface::class && !$connection instanceof $className) {
+            throw new \RuntimeException(
+                \sprintf(
+                    "The queue obtained by the context `%s` doesn't match the type `%s`.",
+                    $context,
+                    $className
+                )
+            );
+        }
+
+        return $connection;
+    }
+}
