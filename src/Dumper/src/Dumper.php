1f3ec039671d105ab17dd98d756340ee34f00899
Maxim Smakouz <m.smakouz@gmail.com>
2022-03-22 21:12:07 +0200

    Update Dumper code with php 8.1 features

--- src/Dumper/src/Dumper.php
+++ src/Dumper/src/Dumper.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Spiral Framework.
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
-
 declare(strict_types=1);
 
 namespace Spiral\Debug;
@@ -37,15 +30,14 @@ class Dumper implements LoggerAwareInterface
     public const OUTPUT_CLI_COLORS = 5;
     public const ROADRUNNER        = 6;
 
-    /** @var int */
-    private $maxLevel = 12;
+    private int $maxLevel = 12;
 
     /**
      * Default render associations.
      *
      * @var array|RendererInterface[]
      */
-    private $targets = [
+    private array $targets = [
         self::OUTPUT            => HtmlRenderer::class,
         self::OUTPUT_CLI        => PlainRenderer::class,
         self::OUTPUT_CLI_COLORS => ConsoleRenderer::class,
@@ -73,12 +65,11 @@ class Dumper implements LoggerAwareInterface
     /**
      * Dump given value into target output.
      *
-     * @param mixed $value
-     * @param int   $target Possible options: OUTPUT, RETURN, ERROR_LOG, LOGGER.
-     * @return string
+     * @param int $target Possible options: OUTPUT, RETURN, ERROR_LOG, LOGGER.
+     *
      * @throws DumperException
      */
-    public function dump($value, int $target = self::OUTPUT): ?string
+    public function dump(mixed $value, int $target = self::OUTPUT): ?string
     {
         $r = $this->getRenderer($target);
         $dump = $r->wrapContent($this->renderValue($r, $value));
@@ -115,7 +106,7 @@ class Dumper implements LoggerAwareInterface
     public function setRenderer(int $target, RendererInterface $renderer): Dumper
     {
         if (!isset($this->targets[$target])) {
-            throw new DumperException(sprintf('Undefined dump target %d', $target));
+            throw new DumperException(\sprintf('Undefined dump target %d', $target));
         }
 
         $this->targets[$target] = $renderer;
@@ -140,10 +131,10 @@ class Dumper implements LoggerAwareInterface
         }
 
         if (!isset($this->targets[$target])) {
-            throw new DumperException(sprintf('Undefined dump target %d', $target));
+            throw new DumperException(\sprintf('Undefined dump target %d', $target));
         }
 
-        if (is_string($this->targets[$target])) {
+        if (\is_string($this->targets[$target])) {
             $this->targets[$target] = new $this->targets[$target]();
         }
 
@@ -154,14 +145,13 @@ class Dumper implements LoggerAwareInterface
      * Variable dumper. This is the oldest spiral function originally written in 2007. :).
      *
      * @param RendererInterface $r          Render to style value content.
-     * @param mixed             $value
      * @param string            $name       Variable name, internal.
      * @param int               $level      Dumping level, internal.
      * @param bool              $hideHeader Hide array/object header, internal.
      */
     private function renderValue(
         RendererInterface $r,
-        $value,
+        mixed $value,
         string $name = '',
         int $level = 0,
         bool $hideHeader = false
@@ -177,33 +167,33 @@ class Dumper implements LoggerAwareInterface
             return $r->indent($level) . $r->apply('-too deep-', 'maxLevel') . "\n";
         }
 
-        $type = strtolower(gettype($value));
+        $type = \strtolower(gettype($value));
 
-        if ($type == 'array') {
+        if ($type === 'array') {
             return $header . $this->renderArray($r, $value, $level, $hideHeader);
         }
 
-        if ($type == 'object') {
+        if ($type === 'object') {
             return $header . $this->renderObject($r, $value, $level, $hideHeader);
         }
 
-        if ($type == 'resource') {
+        if ($type === 'resource') {
             //No need to dump resource value
-            $element = get_resource_type($value) . ' resource ';
+            $element = \get_resource_type($value) . ' resource ';
 
             return $header . $r->apply($element, 'type', 'resource') . "\n";
         }
 
         //Value length
-        $length = strlen((string)$value);
+        $length = \strlen((string)$value);
 
         //Including type size
-        $header .= $r->apply("{$type}({$length})", 'type', $type);
+        $header .= $r->apply(\sprintf('%s(%d)', $type, $length), 'type', $type);
 
         $element = null;
         switch ($type) {
             case 'string':
-                $element = $r->escapeStrings() ? htmlspecialchars($value) : $value;
+                $element = $r->escapeStrings() ? \htmlspecialchars($value) : $value;
                 break;
 
             case 'boolean':
@@ -213,7 +203,7 @@ class Dumper implements LoggerAwareInterface
             default:
                 if ($value !== null) {
                     //Not showing null value, type is enough
-                    $element = var_export($value, true);
+                    $element = \var_export($value, true);
                 }
         }
 
@@ -224,25 +214,25 @@ class Dumper implements LoggerAwareInterface
     private function renderArray(RendererInterface $r, array $array, int $level, bool $hideHeader = false): string
     {
         if (!$hideHeader) {
-            $count = count($array);
+            $count = \count($array);
 
             //Array size and scope
-            $output = $r->apply("array({$count})", 'type', 'array') . "\n";
+            $output = $r->apply(\sprintf('array(%d)', $count), 'type', 'array') . "\n";
             $output .= $r->indent($level) . $r->apply('[', 'syntax', '[') . "\n";
         } else {
             $output = '';
         }
 
         foreach ($array as $key => $value) {
-            if (!is_numeric($key)) {
-                if (is_string($key) && $r->escapeStrings()) {
-                    $key = htmlspecialchars($key);
+            if (!\is_numeric($key)) {
+                if (\is_string($key) && $r->escapeStrings()) {
+                    $key = \htmlspecialchars($key);
                 }
 
-                $key = "'{$key}'";
+                $key = \sprintf("'%s'", $key);
             }
 
-            $output .= $this->renderValue($r, $value, "[{$key}]", $level + 1);
+            $output .= $this->renderValue($r, $value, \sprintf('[%s]', $key), $level + 1);
         }
 
         if (!$hideHeader) {
@@ -253,19 +243,15 @@ class Dumper implements LoggerAwareInterface
         return $output;
     }
 
-    /**
-     * @param object            $value
-     *
-     */
     private function renderObject(
         RendererInterface $r,
-        $value,
+        object $value,
         int $level,
         bool $hideHeader = false,
         string $class = ''
     ): string {
         if (!$hideHeader) {
-            $type = ($class ?: get_class($value)) . ' object ';
+            $type = ($class ?: $value::class) . ' object ';
 
             $header = $r->apply($type, 'type', 'object') . "\n";
             $header .= $r->indent($level) . $r->apply('(', 'syntax', '(') . "\n";
@@ -274,25 +260,25 @@ class Dumper implements LoggerAwareInterface
         }
 
         //Let's use method specifically created for dumping
-        if (method_exists($value, '__debugInfo') || $value instanceof \Closure) {
+        if (\method_exists($value, '__debugInfo') || $value instanceof \Closure) {
             if ($value instanceof \Closure) {
                 $debugInfo = $this->describeClosure($value);
             } else {
                 $debugInfo = $value->__debugInfo();
             }
 
-            if (is_array($debugInfo)) {
+            if (\is_array($debugInfo)) {
                 //Pretty view
                 $debugInfo = (object)$debugInfo;
             }
 
-            if (is_object($debugInfo)) {
+            if (\is_object($debugInfo)) {
                 //We are not including syntax elements here
-                return $this->renderObject($r, $debugInfo, $level, false, get_class($value));
+                return $this->renderObject($r, $debugInfo, $level, false, $value::class);
             }
 
             return $header
-                . $this->renderValue($r, $debugInfo, '', $level + (is_scalar($value)), true)
+                . $this->renderValue($r, $debugInfo, '', $level + (\is_scalar($value)), true)
                 . $r->indent($level) . $r->apply(')', 'syntax', ')') . "\n";
         }
 
@@ -307,20 +293,16 @@ class Dumper implements LoggerAwareInterface
         return $header . $output . $r->indent($level) . $r->apply(')', 'syntax', ')') . "\n";
     }
 
-    /**
-     * @param object              $value
-     *
-     */
-    private function renderProperty(RendererInterface $r, $value, \ReflectionProperty $p, int $level): string
+    private function renderProperty(RendererInterface $r, object $value, \ReflectionProperty $p, int $level): string
     {
         if ($p->isStatic()) {
             return '';
         }
 
         if (
-            !($value instanceof \stdClass)
-            && is_string($p->getDocComment())
-            && strpos($p->getDocComment(), '@internal') !== false
+            !($value instanceof \stdClass) &&
+            \is_string($p->getDocComment()) &&
+            str_contains($p->getDocComment(), '@internal')
         ) {
             // Memory loop while reading doc comment for stdClass variables?
             // Report a PHP bug about treating comment INSIDE property declaration as doc comment.
@@ -329,9 +311,6 @@ class Dumper implements LoggerAwareInterface
 
         //Property access level
         $access = $this->getAccess($p);
-
-        //To read private and protected properties
-        $p->setAccessible(true);
 
         if ($value instanceof \stdClass) {
             $name = $r->apply($p->getName(), 'dynamic');
@@ -350,12 +329,12 @@ class Dumper implements LoggerAwareInterface
     {
         try {
             $r = new \ReflectionFunction($closure);
-        } catch (\ReflectionException $e) {
+        } catch (\ReflectionException) {
             return ['closure' => 'unable to resolve'];
         }
 
         return [
-            'name' => $r->getName() . " (lines {$r->getStartLine()}:{$r->getEndLine()})",
+            'name' => $r->getName() . \sprintf(' (lines %s:%s)', $r->getStartLine(), $r->getEndLine()),
             'file' => $r->getFileName(),
             'this' => $r->getClosureThis(),
         ];
@@ -363,17 +342,13 @@ class Dumper implements LoggerAwareInterface
 
     /**
      * Property access level label.
-     *
-     *
      */
     private function getAccess(\ReflectionProperty $p): string
     {
-        if ($p->isPrivate()) {
-            return 'private';
-        } elseif ($p->isProtected()) {
-            return 'protected';
-        }
-
-        return 'public';
+        return match (true) {
+            $p->isPrivate() => 'private',
+            $p->isProtected() => 'protected',
+            default => 'public'
+        };
     }
 }
--- src/Dumper/src/System.php
+++ src/Dumper/src/System.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Spiral Framework.
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
-
 declare(strict_types=1);
 
 namespace Spiral\Debug;
@@ -23,16 +16,12 @@ final class System
      */
     public static function isCLI(): bool
     {
-        if (!empty(getenv('RR'))) {
+        if (!empty(\getenv('RR'))) {
             // Do not treat RoadRunner as CLI.
             return false;
         }
 
-        if (php_sapi_name() === 'cli') {
-            return true;
-        }
-
-        return false;
+        return PHP_SAPI === 'cli';
     }
 
     /**
@@ -40,26 +29,25 @@ final class System
      *
      * @codeCoverageIgnore
      * @link https://github.com/symfony/Console/blob/master/Output/StreamOutput.php#L94
-     * @param mixed $stream
      */
-    public static function isColorsSupported($stream = STDOUT): bool
+    public static function isColorsSupported(mixed $stream = STDOUT): bool
     {
-        if ('Hyper' === getenv('TERM_PROGRAM')) {
+        if ('Hyper' === \getenv('TERM_PROGRAM')) {
             return true;
         }
 
         try {
             if (\DIRECTORY_SEPARATOR === '\\') {
                 return (
-                        function_exists('sapi_windows_vt100_support')
+                        \function_exists('sapi_windows_vt100_support')
                         && @sapi_windows_vt100_support($stream)
-                    ) || getenv('ANSICON') !== false
-                    || getenv('ConEmuANSI') == 'ON'
-                    || getenv('TERM') == 'xterm';
+                    ) || \getenv('ANSICON') !== false
+                    || \getenv('ConEmuANSI') === 'ON'
+                    || \getenv('TERM') === 'xterm';
             }
 
             return @stream_isatty($stream);
-        } catch (\Throwable $e) {
+        } catch (\Throwable) {
             return false;
         }
     }
