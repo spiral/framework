c4b2bf0201684cc61a83079edc7904fe1c866b42
butschster <butschster@gmail.com>
2022-04-26 14:50:39 +0300

    Fixes problem with cyclic filter resolving.

--- src/Filters/src/FilterProvider.php
+++ src/Filters/src/FilterProvider.php
@@ -9,6 +9,10 @@ use Spiral\Filters\Exception\SchemaException;
 
 final class FilterProvider implements FilterProviderInterface
 {
+    /** @var <class-string, array{0: Filter, 1: array}> */
+    private array $cache = [];
+
+
     public function __construct(
         private readonly Container $container = new Container()
     ) {
@@ -16,17 +20,27 @@ final class FilterProvider implements FilterProviderInterface
 
     public function createFilter(string $name, InputInterface $input): FilterInterface
     {
-        $filter = $this->container->get($name);
+        if (isset($this->cache[$name])) {
+            [$filter, $schema] = $this->cache[$name];
 
-        if (!$filter instanceof Filter) {
-            throw new SchemaException('Invalid filter class, must be subclass of Filter');
+            // Clear old input data
+            $filter->__destruct();
+        } else {
+
+            $class = new \ReflectionClass($name);
+            $args = $this->container->resolveArguments($class->getConstructor());
+            $filter = $class->newInstanceArgs($args);
+
+            if (!$filter instanceof Filter) {
+                throw new SchemaException('Invalid filter class, must be subclass of Filter');
+            }
+
+            $builder = new Schema\Builder();
+            $schema = $builder->makeSchema($name, $filter->mappingSchema());
+            $filter->withErrorMapper(new ErrorMapper($schema));
+
+            $this->cache[$name] = [$filter, $schema];
         }
-
-        $filter->__destruct();
-
-        $builder = new Schema\Builder();
-        $schema = $builder->makeSchema($name, $filter->mappingSchema());
-        $filter->withErrorMapper(new ErrorMapper($schema));
 
         $inputMapper = $this->container->get(Schema\InputMapper::class);
 
