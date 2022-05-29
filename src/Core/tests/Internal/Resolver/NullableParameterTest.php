f1fb760d2d087cb19d137bb5977b16ae723e4345
roxblnfk <roxblnfk@ya.ru>
2022-05-29 22:00:26 +0400

    Cleanup

--- src/Core/tests/Internal/Resolver/NullableParameterTest.php
+++ src/Core/tests/Internal/Resolver/NullableParameterTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Core\Internal\Resolver;
 
-use DateTimeImmutable;
 use DateTimeInterface;
 use RuntimeException;
 use Spiral\Tests\Core\Stub\EngineInterface;
