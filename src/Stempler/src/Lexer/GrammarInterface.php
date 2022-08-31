60bae027a48f084d2a788910eb735e81d3bb1f64
butschster <butschster@gmail.com>
2022-08-31 20:38:22 +0400

    Fixes psalm issues

--- src/Stempler/src/Lexer/GrammarInterface.php
+++ src/Stempler/src/Lexer/GrammarInterface.php
@@ -9,7 +9,7 @@ interface GrammarInterface
     /**
      * Generate stream of tokens or pass generation to overlay grammar.
      *
-     * @return \Generator<array-key, Byte|Token>
+     * @return \Generator<array-key, Byte|Token|null>
      */
     public function parse(Buffer $src): \Generator;
 
