3a39cfe7e6d284da30b866b2aa075973058e2bc0
roxblnfk <roxblnfk@ya.ru>
2022-08-31 11:08:16 +0400

    Some fixes

--- src/Boot/src/Bootloader/Bootloader.php
+++ src/Boot/src/Bootloader/Bootloader.php
@@ -12,12 +12,20 @@ namespace Spiral\Boot\Bootloader;
  *
  * Attention, you are able to define your own set of shared (short bindings) components in your
  * bootloader, DO NOT share your business models this way - use regular DI.
+ *
+ * @phpstan-import-type TConstantBinding from BootloaderInterface
  */
 abstract class Bootloader implements BootloaderInterface, DependedInterface
 {
-    /** @var array<string, class-string|callable> */
+    /**
+     * @var array<string, class-string|callable>
+     * @psalm-var TConstantBinding
+     */
     protected const BINDINGS = [];
-    /** @var array<string, class-string|callable> */
+    /**
+     * @var array<string, class-string|callable>
+     * @psalm-var TConstantBinding
+     */
     protected const SINGLETONS = [];
     /** @var class-string<BootloaderInterface>[]|class-string<DependedInterface>[] */
     protected const DEPENDENCIES = [];
--- src/Boot/src/Bootloader/BootloaderInterface.php
+++ src/Boot/src/Bootloader/BootloaderInterface.php
@@ -9,6 +9,11 @@ namespace Spiral\Boot\Bootloader;
  * they can be cached.
  *
  * To make class bootable (using method boot() with method injections) declare constant BOOT = true;
+ *
+ * @psalm-type TStaticBindingValue = class-string|non-empty-string|array{class-string, non-empty-string}
+ * @psalm-type TContainerBindingValue = TStaticBindingValue|object|callable
+ * @psalm-type TConstantBinding = array<class-string|non-empty-string, TStaticBindingValue>
+ * @psalm-type TFullBinding = array<class-string|non-empty-string, TContainerBindingValue>
  */
 interface BootloaderInterface
 {
@@ -20,7 +25,7 @@ interface BootloaderInterface
      *      'class' => [self::class, 'createMethod']
      * ]
      *
-     * @return array<string, class-string|callable>
+     * @return TFullBinding
      */
     public function defineBindings(): array;
 
@@ -35,7 +40,7 @@ interface BootloaderInterface
      * You don't need to bind classes which are declared with SINGLETON constant here, spiral will
      * resolve them as singleton automatically.
      *
-     * @return array<string, class-string|callable>
+     * @return TFullBinding
      */
     public function defineSingletons(): array;
 }
