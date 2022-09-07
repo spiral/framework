2f7740c91ec116e20b59d2144e18489ded617ff7
roxblnfk <roxblnfk@ya.ru>
2022-09-07 16:48:30 +0400

    Add comment

--- src/Exceptions/src/ExceptionHandler.php
+++ src/Exceptions/src/ExceptionHandler.php
@@ -92,6 +92,7 @@ class ExceptionHandler implements ExceptionHandlerInterface
         // we are safe to handle global exceptions (system level) with maximum verbosity
         $this->report($e);
 
+        // There is possible an exception on the application termination
         try {
             \fwrite($this->output, $this->render($e, verbosity: $this->verbosity, format: $format));
         } catch (\Throwable) {
