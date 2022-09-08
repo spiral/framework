75a8a7fc1a23dd8894bcf94213c8592ada41e3f6
roxblnfk <roxblnfk@ya.ru>
2022-09-08 19:28:53 +0400

    Optimize ListenerProcessor

--- src/Events/src/Processor/AttributeProcessor.php
+++ src/Events/src/Processor/AttributeProcessor.php
@@ -19,12 +19,15 @@ final class AttributeProcessor extends AbstractProcessor
 
     public function process(): void
     {
+        if ($this->registry === null) {
+            return;
+        }
         foreach ($this->locator->findListeners() as $listener => $attr) {
             $method = $this->getMethod($listener, $attr->method ?? '__invoke');
 
             $events = (array)($attr->event ?? $this->getEventFromTypeDeclaration($method));
             foreach ($events as $event) {
-                $this->registry?->addListener(
+                $this->registry->addListener(
                     event: $event,
                     listener: $this->factory->create($listener, $method->getName()),
                     priority: $attr->priority
