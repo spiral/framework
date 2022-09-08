9e3422165123fe242a6a6b75fe7be997af755b1f
Maxim Smakouz <m.smakouz@gmail.com>
2022-09-08 14:04:02 +0300

    Fix line number in tests

--- src/Tokenizer/tests/InvocationsTest.php
+++ src/Tokenizer/tests/InvocationsTest.php
@@ -72,8 +72,8 @@ class InvocationsTest extends TestCase
         $invocation1 = $invocations[0];
         $invocation2 = $invocations[1];
 
-        $this->assertSame(21, $invocation1->getLine());
-        $this->assertSame(22, $invocation2->getLine());
+        $this->assertSame(15, $invocation1->getLine());
+        $this->assertSame(16, $invocation2->getLine());
     }
 
     public function testLevel()
