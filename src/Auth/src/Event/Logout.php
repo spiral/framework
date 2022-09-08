0914e0fdd6c22cd888d686aebe34e2a123daa86e
butschster <butschster@gmail.com>
2022-09-08 23:07:18 +0400

    Fixes Auth Logout event actor type

--- src/Auth/src/Event/Logout.php
+++ src/Auth/src/Event/Logout.php
@@ -7,7 +7,7 @@ namespace Spiral\Auth\Event;
 final class Logout
 {
     public function __construct(
-        public readonly object $actor,
+        public readonly ?object $actor,
         public readonly ?string $transport = null
     ) {
     }
