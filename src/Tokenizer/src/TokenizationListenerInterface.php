48afd35f32d500dfb83d9f0e5c77ac3b648e3f21
butschster <butschster@gmail.com>
2022-09-12 11:07:24 +0400

    Add phpdoc for TokenizationListenerInterface

--- src/Tokenizer/src/TokenizationListenerInterface.php
+++ src/Tokenizer/src/TokenizationListenerInterface.php
@@ -6,7 +6,13 @@ namespace Spiral\Tokenizer;
 
 interface TokenizationListenerInterface
 {
+    /**
+     * The method will be fired for every found class by a class locator.
+     */
     public function listen(\ReflectionClass $class): void;
 
+    /**
+     * The method will be fired after a class locator will finish iterating of found classes.
+     */
     public function finalize(): void;
 }
