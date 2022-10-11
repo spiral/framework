67d089fdbbd9d48093c509523797c6e31cd3292f
Maxim Smakouz <m.smakouz@gmail.com>
2022-10-11 14:47:36 +0300

    Add extending interface

--- src/Queue/src/ExtendedOptionsInterface.php
+++ src/Queue/src/ExtendedOptionsInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Spiral\Queue;
 
-interface ExtendedOptionsInterface
+interface ExtendedOptionsInterface extends OptionsInterface
 {
     /**
      * @return array<non-empty-string, array<string>>
