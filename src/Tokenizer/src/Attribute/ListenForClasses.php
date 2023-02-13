6c6cb0032903887cda5f458c402ba99801ea82ee
butschster <butschster@gmail.com>
2023-02-13 12:04:48 +0400

    Fixes psalm

--- src/Tokenizer/src/Attribute/ListenForClasses.php
+++ src/Tokenizer/src/Attribute/ListenForClasses.php
@@ -10,7 +10,7 @@ use Spiral\Attributes\NamedArgumentConstructor;
  * When applied to a listener, this attribute will instruct the tokenizer to listen for classes that are extending or
  * implementing the given class or uses attributes of the given class.
  */
-#[\Attribute(\Attribute::TARGET_CLASS, \Attribute::IS_REPEATABLE), NamedArgumentConstructor]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE), NamedArgumentConstructor]
 final class ListenForClasses
 {
     /**
