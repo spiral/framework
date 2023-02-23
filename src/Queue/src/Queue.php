2cc314f32e2ed167d2aa4008231a05a5e3e30167
Maxim Smakouz <m.smakouz@gmail.com>
2023-02-23 11:29:14 +0200

    Add the ability to use object payload and options from queue implementations

--- src/Queue/src/Queue.php
+++ src/Queue/src/Queue.php
@@ -13,7 +13,7 @@ final class Queue implements QueueInterface
     ) {
     }
 
-    public function push(string $name, array $payload = [], OptionsInterface $options = null): string
+    public function push(string $name, mixed $payload = [], mixed $options = null): string
     {
         return $this->core->callAction($name, 'push', [
             'payload' => $payload,
