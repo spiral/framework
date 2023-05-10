8c94dc5cb807cff20727754ac670480761738fbe
butschster <butschster@gmail.com>
2023-05-10 15:26:39 +0400

    Fixes unit test

--- tests/Framework/Bootloader/Attributes/AttributesWithoutAnnotationsBootloaderTest.php
+++ tests/Framework/Bootloader/Attributes/AttributesWithoutAnnotationsBootloaderTest.php
@@ -6,17 +6,29 @@ namespace Framework\Bootloader\Attributes;
 
 use Spiral\Attributes\AttributeReader;
 use Spiral\Attributes\ReaderInterface;
+use Spiral\Bootloader\Attributes\AttributesConfig;
 use Spiral\Bootloader\Attributes\Factory;
+use Spiral\Config\ConfiguratorInterface;
 use Spiral\Tests\Framework\BaseTestCase;
 
 final class AttributesWithoutAnnotationsBootloaderTest extends BaseTestCase
 {
-    public const ENV = [
-        'SUPPORT_ANNOTATIONS' => false,
-    ];
+    protected function setUp(): void
+    {
+        $this->beforeInit(function (ConfiguratorInterface $configurator) {
+            $configurator->setDefaults(AttributesConfig::CONFIG, [
+                'annotations' => [
+                    'support' => false,
+                ],
+            ]);
+        });
+
+        parent::setUp();
+    }
 
     public function testReaderBinding(): void
     {
+
         $this->assertContainerBoundAsSingleton(ReaderInterface::class, AttributeReader::class);
     }
 }
