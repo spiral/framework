e53808d955b302f80292367536f50d24591d82df
Maxim Smakouz <m.smakouz@gmail.com>
2023-08-03 13:33:22 +0300

    Add exception description

--- src/Core/src/Exception/Binder/SingletonOverloadException.php
+++ src/Core/src/Exception/Binder/SingletonOverloadException.php
@@ -6,6 +6,10 @@ namespace Spiral\Core\Exception\Binder;
 
 use Spiral\Core\Exception\ConfiguratorException;
 
+/**
+ * Thrown when trying to change a singleton binding that has already been requested.
+ * If it is needed in some cases, need to call the {@see \Spiral\Core\Container::removeBinding()} method before binding.
+ */
 final class SingletonOverloadException extends ConfiguratorException
 {
 }
