f87c9b5dc821408f8596f9d44e9b1b13cb664e6c
Maxim Smakouz <m.smakouz@gmail.com>
2023-08-29 16:49:12 +0300

    Checking `singletons` in the hasInstance method (#975)
    * Check singletons in the hasInstance method
    
    * Improve tests

--- src/Core/tests/Internal/Config/StateBinderTest.php
+++ src/Core/tests/Internal/Config/StateBinderTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Spiral\Tests\Core\Internal\Config;
 
 use Spiral\Core\BinderInterface;
+use Spiral\Core\Container\SingletonInterface;
 use Spiral\Core\Exception\Binder\SingletonOverloadException;
 use Spiral\Core\FactoryInterface;
+use Spiral\Tests\Core\Fixtures\SampleClass;
 use Spiral\Tests\Core\Internal\BaseTestCase;
 
 final class StateBinderTest extends BaseTestCase
@@ -37,5 +39,41 @@ final class StateBinderTest extends BaseTestCase
 
         $this->expectException(SingletonOverloadException::class);
         $binder->bind('test', new \stdClass());
+    }
+
+    public function testHasInstanceAfterMakeWithoutAlias(): void
+    {
+        $binder = $this->constructor->get('binder', BinderInterface::class);
+        $factory = $this->constructor->get('factory', FactoryInterface::class);
+
+        $this->bindSingleton('test', new class implements SingletonInterface {});
+        $factory->make('test');
+
+        $this->assertTrue($binder->hasInstance('test'));
+    }
+
+    public function testHasInstanceAfterMakeWithAlias(): void
+    {
+        $binder = $this->constructor->get('binder', BinderInterface::class);
+        $factory = $this->constructor->get('factory', FactoryInterface::class);
+
+        $this->bindSingleton('test', SampleClass::class);
+        $factory->make('test');
+
+        $this->assertTrue($binder->hasInstance('test'));
+    }
+
+    public function testHasInstanceAfterMakeWithNestedAlias(): void
+    {
+        $binder = $this->constructor->get('binder', BinderInterface::class);
+        $factory = $this->constructor->get('factory', FactoryInterface::class);
+
+        $this->bindSingleton('sampleClass', SampleClass::class);
+        $this->bindSingleton('foo', 'sampleClass');
+
+        $this->bindSingleton('bar', 'foo');
+        $factory->make('bar');
+
+        $this->assertTrue($binder->hasInstance('bar'));
     }
 }
