e5ff4eddd87e82acdf5b8b7a56c85820f6b82be4
Maxim Smakouz <m.smakouz@gmail.com>
2023-09-05 09:33:35 +0300

    Improve Tokenizer Info console command (#979)

--- src/Framework/Command/Tokenizer/InfoCommand.php
+++ src/Framework/Command/Tokenizer/InfoCommand.php
@@ -5,16 +5,14 @@ declare(strict_types=1);
 namespace Spiral\Command\Tokenizer;
 
 use Spiral\Boot\DirectoriesInterface;
-use Spiral\Console\Attribute\AsCommand;
 use Spiral\Console\Command;
 use Spiral\Tokenizer\Config\TokenizerConfig;
 
-#[AsCommand(
-    name: 'tokenizer:info',
-    description: 'Get information about tokenizer directories to scan'
-)]
 final class InfoCommand extends Command
 {
+    protected const NAME = 'tokenizer:info';
+    protected const DESCRIPTION = 'Get information about tokenizer directories to scan';
+
     public function perform(TokenizerConfig $config, DirectoriesInterface $dirs): int
     {
         $this->info('Included directories:');
@@ -46,8 +44,27 @@ final class InfoCommand extends Command
         $grid->render();
 
         $this->newLine();
+
+        $this->info('Loaders:');
+        $grid = $this->table(['Loader', 'Status']);
+
+        $grid->addRow(['Classes', $config->isLoadClassesEnabled()
+            ? '<info>enabled</>'
+            : '<error>disabled</>. <comment>To enable, add "TOKENIZER_LOAD_CLASSES=true" to your .env file.</>']);
+        $grid->addRow(['Enums', $config->isLoadEnumsEnabled()
+            ? '<info>enabled</>'
+            : '<error>disabled</>. <comment>To enable, add "TOKENIZER_LOAD_ENUMS=true" to your .env file.</>']);
+        $grid->addRow(
+            ['Interfaces', $config->isLoadInterfacesEnabled()
+                ? '<info>enabled</>'
+                : '<error>disabled</>. <comment>To enable, add "TOKENIZER_LOAD_INTERFACES=true" to your .env file.</>']
+        );
+
+        $grid->render();
+
+        $this->newLine();
         $this->info(
-            \sprintf('Tokenizer cache: %s', $config->isCacheEnabled() ? '<success>enabled</>' : '<error>disabled</>'),
+            \sprintf('Tokenizer cache: %s', $config->isCacheEnabled() ? '<info>enabled</>' : '<error>disabled</>'),
         );
         if (!$config->isCacheEnabled()) {
             $this->comment('To enable cache, add "TOKENIZER_CACHE_TARGETS=true" to your .env file.');
