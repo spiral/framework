0e23c79725f9c6f17deaf3c47cc50c5147cdc943
Maxim Smakouz <m.smakouz@gmail.com>
2023-12-25 13:50:57 +0200

    Add Listeners to the Tokenizer Info

--- src/Tokenizer/src/TokenizerListenerRegistryInterface.php
+++ src/Tokenizer/src/TokenizerListenerRegistryInterface.php
@@ -6,6 +6,7 @@ namespace Spiral\Tokenizer;
 
 /**
  * It contains all listeners that will be noticed about found classes by a class locator.
+ * @method getListenerClasses(): array
  */
 interface TokenizerListenerRegistryInterface
 {
