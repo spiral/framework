7d08cacc0b12aaa4c88a7dbce97bb0d278b3409e
Maxim Smakouz <m.smakouz@gmail.com>
2024-01-02 19:28:26 +0200

    Fix TelemetryProcessor

--- src/Telemetry/src/Monolog/TelemetryProcessor.php
+++ src/Telemetry/src/Monolog/TelemetryProcessor.php
@@ -18,6 +18,10 @@ final class TelemetryProcessor implements ProcessorInterface
 
     public function __invoke(LogRecord|array $record): array
     {
+        if ($record instanceof LogRecord) {
+            $record = $record->toArray();
+        }
+
         $tracer = $this->container->get(TracerInterface::class);
         \assert($tracer instanceof TracerInterface);
 
