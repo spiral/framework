ad0491643f1059724598e46947404168ddf75136
Maxim Smakouz <m.smakouz@gmail.com>
2024-01-18 10:38:37 +0200

    Remove EnvironmentInterface from canServe method

--- src/Boot/src/DispatcherInterface.php
+++ src/Boot/src/DispatcherInterface.php
@@ -7,7 +7,7 @@ namespace Spiral\Boot;
 /**
  * Dispatchers are general application flow controllers, system should start them and pass exception
  * or instance of snapshot into them when error happens.
- * @method static bool canServe(EnvironmentInterface $env)
+ * @method static bool canServe()
  */
 interface DispatcherInterface
 {
