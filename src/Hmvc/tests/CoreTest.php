a82dcfe94ce05cf302562ba1125b1b27320faa0b
Maxim Smakouz <m.smakouz@gmail.com>
2024-01-25 15:41:55 +0200

    Simplify Hmvc tests

--- src/Hmvc/tests/CoreTest.php
+++ src/Hmvc/tests/CoreTest.php
@@ -4,235 +4,207 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Core;
 
-use PHPUnit\Framework\TestCase;
-use Spiral\Core\Container;
 use Spiral\Core\Exception\ControllerException;
-use Spiral\Core\Scope;
 use Spiral\Framework\ScopeName;
+use Spiral\Testing\Attribute\TestScope;
+use Spiral\Testing\TestCase;
 use Spiral\Tests\Core\Fixtures\CleanController;
 use Spiral\Tests\Core\Fixtures\DummyController;
 use Spiral\Tests\Core\Fixtures\SampleCore;
 
 final class CoreTest extends TestCase
 {
-    private Container $root;
-
-    protected function setUp(): void
-    {
-        $this->root = new Container();
-    }
-
+    #[TestScope(ScopeName::Http)]
     public function testCallAction(): void
     {
-        $core = new SampleCore($this->root);
-
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('Hello, Antony.', $core->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('Hello, Antony.', $core->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallActionDefaultParameter(): void
     {
-        $core = new SampleCore($this->root);
-
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('Hello, Dave.', $core->callAction(
-                DummyController::class,
-                'index'
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('Hello, Dave.', $core->callAction(
+            DummyController::class,
+            'index'
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallActionDefaultAction(): void
     {
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('Hello, Dave.', $core->callAction(
-                DummyController::class,
-                'index'
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('Hello, Dave.', $core->callAction(
+            DummyController::class,
+            'index'
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallActionDefaultActionWithParameter(): void
     {
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('Hello, Antony.', $core->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('Hello, Antony.', $core->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallActionMissingParameter(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'required');
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'required');
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallActionInvalidParameter(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'required', ['id' => null]);
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'required', ['id' => null]);
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallWrongController(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(BadController::class, 'index', ['name' => 'Antony']);
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(BadController::class, 'index', ['name' => 'Antony']);
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallBadAction(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'missing', [
-                'name' => 'Antony',
-            ]);
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'missing', [
+            'name' => 'Antony',
+        ]);
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testStaticAction(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'inner');
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'inner');
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testInheritedAction(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'execute');
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'execute');
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testInheritedActionCall(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(DummyController::class, 'call');
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(DummyController::class, 'call');
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCallNotController(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $core->callAction(SampleCore::class, 'index', [
-                'name' => 'Antony',
-            ]);
-        });
+        $core = new SampleCore($this->getContainer());
+        $core->callAction(SampleCore::class, 'index', [
+            'name' => 'Antony',
+        ]);
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCleanController(): void
     {
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'test',
-                ['id' => '900']
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'test',
+            ['id' => '900']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCleanControllerError(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'test',
-                ['id' => null]
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'test',
+            ['id' => null]
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCleanControllerError2(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'test',
-                []
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'test',
+            []
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCleanControllerError3(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'invalid',
-                []
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'invalid',
+            []
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testCleanControllerError4(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'another',
-                []
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'another',
+            []
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testMissingDependency(): void
     {
         $this->expectException(ControllerException::class);
 
-        $core = new SampleCore($this->root);
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($core): void {
-            $this->assertSame('900', $core->callAction(
-                CleanController::class,
-                'missing',
-                []
-            ));
-        });
+        $core = new SampleCore($this->getContainer());
+        $this->assertSame('900', $core->callAction(
+            CleanController::class,
+            'missing',
+            []
+        ));
     }
 }
--- src/Hmvc/tests/InterceptableCoreTest.php
+++ src/Hmvc/tests/InterceptableCoreTest.php
@@ -4,82 +4,66 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Core;
 
-use PHPUnit\Framework\TestCase;
-use Spiral\Core\Container;
 use Spiral\Core\Exception\InterceptorException;
 use Spiral\Core\InterceptableCore;
 use Spiral\Core\InterceptorPipeline;
-use Spiral\Core\Scope;
 use Spiral\Framework\ScopeName;
+use Spiral\Testing\Attribute\TestScope;
+use Spiral\Testing\TestCase;
 use Spiral\Tests\Core\Fixtures\DummyController;
 use Spiral\Tests\Core\Fixtures\SampleCore;
 
 final class InterceptableCoreTest extends TestCase
 {
-    private Container $root;
-
-    protected function setUp(): void
-    {
-        $this->root = new Container();
-    }
-
+    #[TestScope(ScopeName::Http)]
     public function testNoInterceptors(): void
     {
-        $core = new SampleCore($this->root);
-        $int = new InterceptableCore($core);
+        $int = new InterceptableCore(new SampleCore($this->getContainer()));
 
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($int): void {
-            $this->assertSame('Hello, Antony.', $int->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            ));
-        });
+        $this->assertSame('Hello, Antony.', $int->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testNoInterceptors2(): void
     {
-        $core = new SampleCore($this->root);
-        $int = new InterceptableCore($core);
+        $int = new InterceptableCore(new SampleCore($this->getContainer()));
         $int->addInterceptor(new DemoInterceptor());
 
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($int): void {
-            $this->assertSame('?Hello, Antony.!', $int->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            ));
-        });
+        $this->assertSame('?Hello, Antony.!', $int->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testNoInterceptors22(): void
     {
-        $core = new SampleCore($this->root);
-        $int = new InterceptableCore($core);
+        $int = new InterceptableCore(new SampleCore($this->getContainer()));
         $int->addInterceptor(new DemoInterceptor());
         $int->addInterceptor(new DemoInterceptor());
 
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($int): void {
-            $this->assertSame('??Hello, Antony.!!', $int->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            ));
-        });
+        $this->assertSame('??Hello, Antony.!!', $int->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        ));
     }
 
+    #[TestScope(ScopeName::Http)]
     public function testInvalidPipeline(): void
     {
         $this->expectException(InterceptorException::class);
 
         $pipeline = new InterceptorPipeline();
-
-        $this->root->runScope(new Scope(ScopeName::Http), function () use ($pipeline): void {
-            $pipeline->callAction(
-                DummyController::class,
-                'index',
-                ['name' => 'Antony']
-            );
-        });
+        $pipeline->callAction(
+            DummyController::class,
+            'index',
+            ['name' => 'Antony']
+        );
     }
 }
