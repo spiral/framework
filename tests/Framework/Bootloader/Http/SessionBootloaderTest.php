b7e48f4af301f7bbad7cfc7e264007cdd9cf1cb8
Maxim Smakouz <m.smakouz@gmail.com>
2024-02-20 15:24:56 +0200

    Add unit test for SessionFactoryInterface binding in root scope

--- tests/Framework/Bootloader/Http/SessionBootloaderTest.php
+++ tests/Framework/Bootloader/Http/SessionBootloaderTest.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace Framework\Bootloader\Http;
 
+use Spiral\Framework\Spiral;
 use Spiral\Session\SessionFactory;
 use Spiral\Session\SessionFactoryInterface;
+use Spiral\Testing\Attribute\TestScope;
 use Spiral\Tests\Framework\BaseTestCase;
 
 final class SessionBootloaderTest extends BaseTestCase
 {
+    #[TestScope(Spiral::Http)]
     public function testSessionFactoryInterfaceBinding(): void
     {
         $this->assertContainerBoundAsSingleton(SessionFactoryInterface::class, SessionFactory::class);
+    }
+
+    public function testSessionFactoryInterfaceBindingInRootScope(): void
+    {
+        $this->assertContainerBoundAsSingleton(SessionFactoryInterface::class, SessionFactoryInterface::class);
     }
 }
