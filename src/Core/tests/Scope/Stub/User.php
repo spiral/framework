e458c401ac648c52ba622cf6942d503da591566e
Maxim Smakouz <m.smakouz@gmail.com>
2024-03-07 12:42:47 +0200

    Add mixed param type in proxy

---
+++ src/Core/tests/Scope/Stub/User.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Core\Scope\Stub;
+
+final class User implements UserInterface
+{
+    public function __construct(
+        private string $name,
+    ) {
+    }
+
+    public function setName(string|\Stringable $name): void
+    {
+        $this->name = (string) $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
