414ee75448bc87a620c51c20f62b17b97a5b61f3
roxblnfk <roxblnfk@ya.ru>
2024-05-22 18:53:22 +0400

    TargetAttribute: add $scanParents parameter

---
+++ src/Tokenizer/tests/Interfaces/Targets/AbstractClassWithAttributeOnClass.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Tokenizer\Interfaces\Targets;
+
+use Spiral\Tests\Tokenizer\Fixtures\Attributes\WithTargetClass;
+
+#[WithTargetClass]
+abstract class AbstractClassWithAttributeOnClass
+{
+}
