0eabd1c818bc2be446440048de592f905756cdfb
gam6itko <gam6itko@gmail.com>
2024-08-08 16:53:00 +0300

    remove 'The component is under development.'

--- src/Telemetry/src/TracerFactoryInterface.php
+++ src/Telemetry/src/TracerFactoryInterface.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Spiral\Telemetry;
 
 /**
- * The component is under development.
- *
  * Something may be changed in the future. We will stable it soon.
  * Feedback is welcome {@link https://github.com/spiral/framework/discussions/822}.
  */
--- src/Telemetry/src/TracerInterface.php
+++ src/Telemetry/src/TracerInterface.php
@@ -7,8 +7,6 @@ namespace Spiral\Telemetry;
 use Spiral\Core\ScopeInterface;
 
 /**
- * The component is under development.
- *
  * Something may be changed in the future. We will stable it soon.
  * Feedback is welcome {@link https://github.com/spiral/framework/discussions/822}.
  */
