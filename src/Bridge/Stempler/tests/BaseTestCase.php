dcd438d50945be1eda2e629217ac4ce5170d870f
roxblnfk <roxblnfk@ya.ru>
2024-09-11 20:06:41 +0400

    Update SendIt tests

--- src/Bridge/Stempler/tests/BaseTestCase.php
+++ src/Bridge/Stempler/tests/BaseTestCase.php
@@ -4,25 +4,11 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Stempler;
 
-use Spiral\Boot\BootloadManager\StrategyBasedBootloadManager;
-use Spiral\Boot\BootloadManager\DefaultInvokerStrategy;
-use Spiral\Boot\BootloadManager\Initializer;
-use Spiral\Boot\Directories;
-use Spiral\Boot\DirectoriesInterface;
-use Spiral\Boot\Environment;
-use Spiral\Boot\EnvironmentInterface;
-use Spiral\Config\ConfigManager;
-use Spiral\Config\ConfiguratorInterface;
-use Spiral\Config\Loader\DirectoryLoader;
-use Spiral\Config\Loader\JsonLoader;
-use Spiral\Config\Loader\PhpLoader;
-use Spiral\Core\ConfigsInterface;
 use Spiral\Core\Container;
 use Spiral\Stempler\Bootloader\PrettyPrintBootloader;
 use Spiral\Stempler\Bootloader\StemplerBootloader;
 use Spiral\Stempler\StemplerEngine;
 use Spiral\Testing\TestCase;
-use Spiral\Views\ViewManager;
 use Spiral\Views\ViewsInterface;
 
 abstract class BaseTestCase extends TestCase
