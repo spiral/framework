79b4c2bce9475e0d5a96409a583309ad212468b5
roxblnfk <roxblnfk@ya.ru>
2024-09-21 00:30:28 +0400

    Fix DebugBootloader: unresolved collectors don't break state populating

---
+++ tests/Framework/Bootloader/Debug/ScopedDebugBootloaderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Bootloader\Debug;
+
+use Spiral\Bootloader\DebugBootloader;
+use Spiral\Config\ConfigManager;
+use Spiral\Config\LoaderInterface;
+use Spiral\Debug\Config\DebugConfig;
+use Spiral\Debug\StateInterface;
+use Spiral\Testing\TestCase;
+
+final class ScopedDebugBootloaderTest extends TestCase
+{
+    public function defineBootloaders(): array
+    {
+        return [
+            DebugBootloader::class,
+        ];
+    }
+
+    public function testAddCollectorFromOtherScope(): void
+    {
+        $configs = new ConfigManager($this->createMock(LoaderInterface::class));
+        $configs->setDefaults(DebugConfig::CONFIG, ['collectors' => []]);
+
+        $bootloader = $this->getContainer()->get(DebugBootloader::class);
+        $bootloader->addStateCollector('my-collector');
+        $bootloader->addStateCollector('yet-another-collector');
+
+        $state = $this->getContainer()->get(StateInterface::class);
+
+        self::assertInstanceOf(StateInterface::class, $state);
+        self::assertSame('my-collector, yet-another-collector', $state->getTags()['unresolved-collectors']);
+    }
+}
