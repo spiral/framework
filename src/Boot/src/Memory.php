spiral/framework

commit bbff43634ac102e5ef2b273e64e258b5fc34a218

Fix concurrent writing and reading on workers boot (#1137)
Co-authored-by: Aleksei Gagarin <roxblnfk@ya.ru>

src/Boot/src/Memory.php
@@ -35,7 +35,14 @@ final class Memory implements MemoryInterface
         }
 
         try {
-            return include($filename);
+            $fp = \fopen($filename, 'r');
+            if (!\flock($fp, \LOCK_SH | \LOCK_NB)) {
+                return null;
+            }
+            $data = include($filename);
+            \flock($fp, \LOCK_UN);
+            \fclose($fp);
+            return $data;
         } catch (\Throwable) {
             return null;
         }

commit d2881dd2c2d2c0d5529182231987e926b491e582

fix: boot resource leakage
Introduced in #1137

src/Boot/src/Memory.php
@@ -36,16 +36,27 @@ final class Memory implements MemoryInterface
 
         try {
             $fp = \fopen($filename, 'r');
-            if (!\flock($fp, \LOCK_SH | \LOCK_NB)) {
+            if ($fp === false) {
                 return null;
             }
+
+            if (!\flock($fp, \LOCK_SH | \LOCK_NB)) {
+                \fclose($fp);
+                return null;
+            }
+
             $data = include($filename);
+
             \flock($fp, \LOCK_UN);
-            \fclose($fp);
-            return $data;
         } catch (\Throwable) {
             return null;
+        } finally {
+            if (isset($fp)) {
+                \fclose($fp);
+            }
         }
+
+        return $data;
     }
 
     public function saveData(string $section, mixed $data): void