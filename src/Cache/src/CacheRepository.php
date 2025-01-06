8d91f6ab01e9ae00597ea9a43feba36078951e4b
Pavel Buchnev <butschster@gmail.com>
2025-01-06 16:59:22 +0400

    Optimize multiple cache records (#1194)

--- src/Cache/src/CacheRepository.php
+++ src/Cache/src/CacheRepository.php
@@ -24,9 +24,8 @@ class CacheRepository implements CacheInterface
     public function __construct(
         protected CacheInterface $storage,
         protected ?EventDispatcherInterface $dispatcher = null,
-        protected ?string $prefix = null
-    ) {
-    }
+        protected ?string $prefix = null,
+    ) {}
 
     public function get(string $key, mixed $default = null): mixed
     {
@@ -88,36 +87,84 @@ class CacheRepository implements CacheInterface
 
     public function getMultiple(iterable $keys, mixed $default = null): iterable
     {
-        $result = [];
-
+        $array = [];
+        // Resolve keys and dispatch events
         foreach ($keys as $key) {
-            $result[$key] = $this->get($key, $default);
+            $key = $this->resolveKey($key);
+            $this->dispatcher?->dispatch(new CacheRetrieving($key));
+            // Fill resulting array with default values
+            $array[$key] = $default;
         }
 
-        return $result;
+        // If no dispatcher is set, we can skip the loop with events
+        // to save some CPU cycles
+        $keys = \array_keys($array);
+        if ($this->dispatcher === null) {
+            return $this->storage->getMultiple($keys, $default);
+        }
+
+        $result = $this->storage->getMultiple($keys);
+
+        foreach ($result as $key => $value) {
+            if ($value === null) {
+                $this->dispatcher->dispatch(new CacheMissed($key));
+            } else {
+                // Replace default value with actual value in the resulting array
+                $array[$key] = $value;
+                $this->dispatcher->dispatch(new CacheHit($key, $value));
+            }
+        }
+
+        return $array;
     }
 
     public function setMultiple(iterable $values, \DateInterval|int|null $ttl = null): bool
     {
-        $state = null;
-
+        $dispatcher = $this->dispatcher;
+        $array = [];
+        // Resolve keys and dispatch events
         foreach ($values as $key => $value) {
-            $result = $this->set($key, $value, $ttl);
-            $state = \is_null($state) ? $result : $result && $state;
+            $key = $this->resolveKey($key);
+            $dispatcher?->dispatch(new KeyWriting($key, $value));
+            $array[$key] = $value;
         }
 
-        return $state ?: false;
+        $result = $this->storage->setMultiple($array, $ttl);
+
+        // If there is a dispatcher, we need to dispatch events for each key
+        $dispatcher === null or \array_walk(
+            $array,
+            $result
+                ? static fn(mixed $value, string $key) => $dispatcher->dispatch(new KeyWritten($key, $value))
+                : static fn(mixed $value, string $key) => $dispatcher->dispatch(new KeyWriteFailed($key, $value)),
+        );
+
+        return $result;
     }
 
     public function deleteMultiple(iterable $keys): bool
     {
-        $state = null;
+        $dispatcher = $this->dispatcher;
+
+        $array = [];
+        // Resolve keys and dispatch events
         foreach ($keys as $key) {
-            $result = $this->delete($key);
-            $state = \is_null($state) ? $result : $result && $state;
+            $key = $this->resolveKey($key);
+            $dispatcher?->dispatch(new KeyDeleting($key));
+            $array[] = $key;
         }
 
-        return $state ?: false;
+        $result = $this->storage->deleteMultiple($array);
+
+        // If there is a dispatcher, we need to dispatch events for each key
+        $dispatcher === null or \array_walk(
+            $array,
+            $result
+                ? static fn(string $key) => $dispatcher->dispatch(new KeyDeleted($key))
+                : static fn(string $key) => $dispatcher->dispatch(new KeyDeleteFailed($key)),
+        );
+
+        return $result;
     }
 
     public function has(string $key): bool
