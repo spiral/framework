973841291d34fc91831cacf6e9aab16548dabc66
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-05 17:33:56 +0700

    comment skip

--- structarmed.php
+++ structarmed.php
@@ -7,6 +7,8 @@ use Boundwize\StructArmed\Preset\Preset;
 
 return Architecture::define()
     ->skipPaths([
+        // per src/ has own autoload-dev which needs to be tested separately
+        // if needed
         'src/*/tests',
     ])
     ->withPreset(Preset::PSR4());
