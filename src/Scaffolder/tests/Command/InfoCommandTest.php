49ba5ed7c414900c0700dceca690fd47a20b4930
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-13 16:36:10 +0700

    Fix cs remove unnecessary use statements (#1273)

--- src/Scaffolder/tests/Command/InfoCommandTest.php
+++ src/Scaffolder/tests/Command/InfoCommandTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Scaffolder\Command;
 
-use Spiral\Tests\Scaffolder\Command\AbstractCommandTestCase;
-
 final class InfoCommandTest extends AbstractCommandTestCase
 {
     public function testInfo(): void
