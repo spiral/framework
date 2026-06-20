b372801a8498a9bfc4dc9e9e333d33831e79d65e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-21 06:47:46 +0700

    Enable Rector TypeCoverageLevel 16

--- rector.php
+++ rector.php
@@ -147,7 +147,7 @@ return RectorConfig::configure()
     ->withConfiguredRule(ClassPropertyAssignToConstructorPromotionRector::class, [
         ClassPropertyAssignToConstructorPromotionRector::RENAME_PROPERTY => false,
     ])
-    ->withTypeCoverageLevel(14)
+    ->withTypeCoverageLevel(16)
     ->withRules([
         ClosureReturnTypeRector::class,
         TypedPropertyFromStrictSetUpRector::class,
